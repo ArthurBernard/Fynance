c81baa77a4f621fe1896436c8e9fa3c68e44595c
ArthurBernard <arthur.bernard.92@gmail.com>
2019-01-28 09:59:55 +0100

    fix module name

--- fynance/__init__.py
+++ fynance/__init__.py
@@ -13,16 +13,22 @@ from . import estimator
 from .estimator import *
 from . import tools
 from .tools import *
-from . import neural_network
-from .neural_network import *
+from . import neural_networks
+from .neural_networks import *
 from . import backtest
 from .backtest import *
 
-__all__ = [
-    'get_parameters', 'MA', 'ARMA', 'ARMA_GARCH', 'ARMAX_GARCH', # models
-    'estimator', # not ready
-    'sharpe', 'roll_sharpe', 'sma', 'wma', 'ema', 'smstd', 'wmstd', 'emstd', 
-    'z_score', 'rsi', 'bollinger_band', 'hma', 'macd_line', 'signal_line', 
-    'macd_hist', 'mdd', 'calmar', 'roll_mdd', 'roll_calmar', 'drawdown', # tools 
-    'RollNeuralNet', # neural_network
-]
+__all__ = models.__all__
+__all__ += estimator.__all__
+__all__ += tools.__all__
+__all__ += neural_networks.__all__
+__all__ += backtest.__all__
+
+#__all__ = [
+#    'get_parameters', 'MA', 'ARMA', 'ARMA_GARCH', 'ARMAX_GARCH', # models
+#    'estimator', # not ready
+#    'sharpe', 'roll_sharpe', 'sma', 'wma', 'ema', 'smstd', 'wmstd', 'emstd', 
+#    'z_score', 'rsi', 'bollinger_band', 'hma', 'macd_line', 'signal_line', 
+#    'macd_hist', 'mdd', 'calmar', 'roll_mdd', 'roll_calmar', 'drawdown', # tools 
+#    'RollNeuralNet', # neural_network
+#]
--- fynance/neural_networks/__init__.py
+++ fynance/neural_networks/__init__.py
@@ -8,5 +8,7 @@ Some models, tools, etc for neural network
 """
 from . import roll_neural_network
 from .roll_neural_network import RollNeuralNet
+from . import roll_multi_neural_networks
+from .roll_multi_neural_network import RollMultiNeuralNet
 
-__all__ = ['RollNeuralNet']
+__all__ = ['RollNeuralNet', 'RollMultiNeuralNet']
