2d2bed37f9831e5008971cd3a044fa807b444129
ArthurBernard <arthur.bernard.92@gmail.com>
2019-01-28 12:49:16 +0100

    fix import

--- fynance/neural_networks/__init__.py
+++ fynance/neural_networks/__init__.py
@@ -9,6 +9,6 @@ Some models, tools, etc for neural network
 from . import roll_neural_network
 from .roll_neural_network import RollNeuralNet
 from . import roll_multi_neural_networks
-from .roll_multi_neural_network import RollMultiNeuralNet
+from .roll_multi_neural_networks import RollMultiNeuralNet
 
 __all__ = ['RollNeuralNet', 'RollMultiNeuralNet']
