891a7dca369848a0777aa17d5456a570fb11a719
ArthurBernard <arthur.bernard.92@gmail.com>
2019-01-31 00:08:06 +0100

    clean setup file

--- setup.py
+++ setup.py
@@ -4,79 +4,90 @@
 # Set this to True to enable building extensions using Cython.
 # Set it to False to build extensions from the C file (that
 # was previously created using Cython).
-# Set it to 'auto' to build with Cython if available, otherwise
-# from the C file.
-USE_CYTHON = 'auto'
+USE_CYTHON = False
 
-
-from distutils.core import setup
-from setuptools import find_packages
+import sys
+from setuptools import setup, find_packages
 from distutils.extension import Extension
 import numpy
 
-if USE_CYTHON or USE_CYTHON == 'auto':
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Financial and Insurance Industry',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: POSIX :: Linux',
+    'Programming Language :: Python',
+    'Programming Language :: Cython',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Topic :: Office/Business :: Financial',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+]
+
+MAJOR = 1
+MINOR = 0
+PATCH = 4
+VERSION = '{}.{}.{}'.format(MAJOR, MINOR, PATCH)
+
+DESCRIPTION = 'Python and Cython scripts of machine learning, econometrics ' 
+DESCRIPTION += 'and statistical tools for financial analysis [In progress]'
+
+build_requires = [
+    'matplotlib>=3.0.1',
+    'numpy>=1.15.3',
+    'pandas>=0.23.4',
+    'scipy>=1.2.0',
+    'seaborn>=0.9.0',
+]
+
+if USE_CYTHON:
     try:
         from Cython.Build import cythonize
         from Cython.Distutils import build_ext
-        print('Installation using cython')
+        ext = '.pyx'
+        print('Using cython.')
     except ImportError:
-        if USE_CYTHON == 'auto':
-            USE_CYTHON = False
-            print("Installation not using cython")
-        else:
-            print("Install cython or set USE_CYTHON = 'auto'")
-            raise
+        print("If USE_CYTHON is set to True, Cython is required to compile",  
+            "fynance. Please install Cython or don't set USE_CYTHON to True.")
+        ext = '.c'
+        USE_CYTHON = False
+else:
+    ext = '.c'
 
-cmdclass = { }
+if 'build_ext' in sys.argv[1:] or USE_CYTHON:
+    cmdclass = {'build_ext': build_ext}
+else:
+    cmdclass = {}
+
+extensions = [
+    Extension(
+        'fynance.models.econometric_models_cy', 
+        ['fynance/models/econometric_models_cy' + ext],
+        include_dirs=[numpy.get_include(), '.']
+    ),
+    Extension(
+        'fynance.tools.metrics_cy', 
+        ['fynance/tools/metrics_cy' + ext],
+        include_dirs=[numpy.get_include(), '.']
+    ),
+    Extension(
+        'fynance.tools.momentums_cy', 
+        ['fynance/tools/momentums_cy' + ext],
+        include_dirs=[numpy.get_include(), '.']
+    ),
+    Extension(
+        'fynance.estimator.estimator_cy', 
+        ['fynance/estimator/estimator_cy' + ext],
+        include_dirs=[numpy.get_include(), '.']
+    ),
+]
 
 if USE_CYTHON:
-    extensions = [
-        Extension(
-            "fynance.models.econometric_models_cy", 
-            ["fynance/models/econometric_models_cy.pyx"],
-            include_dirs=[numpy.get_include(), '.']
-        ),
-        Extension(
-            "fynance.tools.metrics_cy", 
-            ["fynance/tools/metrics_cy.pyx"],
-            include_dirs=[numpy.get_include(), '.']
-        ),
-        Extension(
-            "fynance.tools.momentums_cy", 
-            ["fynance/tools/momentums_cy.pyx"],
-            include_dirs=[numpy.get_include(), '.']
-        ),
-        Extension(
-            "fynance.estimator.estimator_cy", 
-            ["fynance/estimator/estimator_cy.pyx"],
-            include_dirs=[numpy.get_include(), '.']
-        ),
-    ]
     ext_modules = cythonize(extensions, annotate=True)
-    cmdclass.update({ 'build_ext': build_ext })
 else:
-    extensions = [
-        Extension(
-            "fynance.models.econometric_models_cy", 
-            ["fynance/models/econometric_models_cy.c"],
-            include_dirs=[numpy.get_include(), '.']
-        ),
-        Extension(
-            "fynance.tools.metrics_cy", 
-            ["fynance/tools/metrics_cy.c"],
-            include_dirs=[numpy.get_include(), '.']
-        ),
-        Extension(
-            "fynance.tools.momentums_cy", 
-            ["fynance/tools/momentums_cy.c"],
-            include_dirs=[numpy.get_include(), '.']
-        ),
-        Extension(
-            "fynance.estimator.estimator_cy", 
-            ["fynance/estimator/estimator_cy.c"],
-            include_dirs=[numpy.get_include(), '.']
-        ),
-    ]
     ext_modules = extensions
 
 with open('README.rst') as f:
@@ -84,37 +95,17 @@ with open('README.rst') as f:
 
 setup(
     name='fynance',
-    version='1.0.1',
-    description='Python and Cython scripts of machine learning, econometrics \
-        and statistical tools for financial analysis [In progress]',
+    version=VERSION,
+    description=DESCRIPTION,
     long_description=str(long_description),
+    license='MIT',
     url='https://github.com/ArthurBernard/Fynance',
+    download_url='https://pypi.org/project/fynance/',
     author=['Arthur Bernard'],
     author_email='arthur.bernard.92@gmail.com',
     packages=find_packages(), #['fynance'],
     cmdclass=cmdclass,
     ext_modules=ext_modules,
-    install_requires=[
-        'matplotlib>=3.0.1',
-        'numpy>=1.15.3',
-        'pandas>=0.23.4',
-        'Cython>=0.29',
-        'scipy>=1.2.0',
-        'seaborn>=0.9.0'
-    ],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Financial and Insurance Industry',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python',
-        'Programming Language :: Cython',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Office/Business :: Financial',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
-    ],
+    install_requires=build_requires,
+    classifiers=CLASSIFIERS,
 )
