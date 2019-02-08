b2a5539bd873c3533bc2f79e50006418514d7956
ArthurBernard <arthur.bernard.92@gmail.com>
2019-02-08 19:38:34 +0100

    remove neural network tools

--- fynance/neural_networks/__init__.py
+++ fynance/neural_networks/__init__.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 """ 
@@ -12,8 +12,8 @@ from . import roll_multi_neural_networks
 from .roll_multi_neural_networks import RollMultiNeuralNet
 from . import roll_aggregated_multi_neural_networks
 from .roll_aggregated_multi_neural_networks import RollAggrMultiNeuralNet
-from . import set_neuralnetwork_tools
-from .set_neuralnetwork_tools import *
+#from . import set_neuralnetwork_tools
+#from .set_neuralnetwork_tools import *
 
 __all__ = ['RollNeuralNet', 'RollMultiNeuralNet', 'RollAggrMultiNeuralNet']
-__all__ += set_neuralnetwork_tools.__all__
+#__all__ += set_neuralnetwork_tools.__all__
