8499c8911faf739cbde13b08fac6b876ae72b998
ArthurBernard <arthur.bernard.92@gmail.com>
2019-02-08 19:29:39 +0100

    fix init files

--- fynance/backtest/__init__.py
+++ fynance/backtest/__init__.py
@@ -11,6 +11,12 @@ from . import print_stats
 from .print_stats import *
 from . import plot_tools
 from .plot_tools import *
+from . import plot_backtest
+from .plot_backtest import *
+from . import dynamic_plot_backtest
+from .dynamic_plot_backtest import *
 
 __all__ = print_stats.__all__
 __all__ += plot_tools.__all__
+__all__ += plot_backtest.__all__
+__all__ += dynamic_plot_backtest.__all__
--- fynance/neural_networks/__init__.py
+++ fynance/neural_networks/__init__.py
@@ -10,5 +10,10 @@ from . import roll_neural_network
 from .roll_neural_network import RollNeuralNet
 from . import roll_multi_neural_networks
 from .roll_multi_neural_networks import RollMultiNeuralNet
+from . import roll_aggregated_multi_neural_networks
+from .roll_aggregated_multi_neural_networks import RollAggrMultiNeuralNet
+from . import set_neuralnetwork_tools
+from .set_neuralnetwork_tools import *
 
-__all__ = ['RollNeuralNet', 'RollMultiNeuralNet']
+__all__ = ['RollNeuralNet', 'RollMultiNeuralNet', 'RollAggrMultiNeuralNet']
+__all__ += set_neuralnetwork_tools.__all__
