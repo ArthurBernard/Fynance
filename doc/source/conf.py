1b50531d6451a80128cfa5b462b45ecf1d5b7aa7
ArthurBernard <arthur.bernard.92@gmail.com>
2019-02-15 12:04:38 +0100

    fix doc configuration

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -20,24 +20,31 @@ extensions = [
 project = 'fynance'
 copyright = '2018-2019, Arthur Bernard'
 author = 'Arthur Bernard'
-version = "1.0"
-release = "1.0.4"
+
+import fynance as fy
+
+version = fy.__version__
+release = fy.__version__
+
 templates_path = ['_templates']
 source_suffix = '.rst'
 master_doc = 'index'
 pygments_style = 'sphinx'
+
+add_function_parentheses = False
+
 html_theme = 'sphinx_rtd_theme'
 html_theme_option = {
 	'display_version': True,
     'prev_next_buttons_location': 'both',
     'style_external_links': True,
     'vcs_pageview_mode' : '',
-    'style_nav_header_background': 'black',
+    'style_nav_header_background': 'white',
     # Toc options
     'collapse_navigation': False,
     'sticky_navigation': True,
     'navigation_depth': 4,
-    'includehidden': False,
+    'includehidden': True,
     'titles_only': False,
     'github_url': 'https://github.com/ArthurBernard/Fynance',
 }
