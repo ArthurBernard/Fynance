59c636897f0984a59c877cff7f9746d6febf0c3d
ArthurBernard <arthur.bernard.92@gmail.com>
2019-02-18 09:36:29 +0100

    fix error import

--- fynance/models/__init__.py
+++ fynance/models/__init__.py
@@ -13,5 +13,4 @@ from . import econometric_models_cy
 from .econometric_models_cy import *
 
 __all__ = econometric_models.__all__
-__all__ += ['econometric_models_cy']
 __all__ += econometric_models_cy.__all__
