2de0639d85dcffae7fcf6241b38cf2cb15c15131
ArthurBernard <arthur.bernard.92@gmail.com>
2019-02-18 16:59:20 +0100

    add abs path

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -4,6 +4,7 @@ import sys
 from sphinx.ext.autosummary import _import_by_name
 from numpydoc.docscrape import NumpyDocString
 from numpydoc.docscrape_sphinx import SphinxDocString
+import importlib
 
 # Check Sphinx version
 import sphinx
@@ -19,7 +20,7 @@ needs_sphinx = '1.6'
 sys.path.insert(0, os.path.abspath('../../'))
 sys.path.insert(0, os.path.abspath('./'))
 sys.path.insert(0, os.path.abspath('../'))
-sys.path.insert(0, os.path.abspath('./fynance/'))
+sys.path.insert(0, os.path.abspath('./fynance/models/'))
 
 extensions = [
     'sphinx.ext.autodoc',
