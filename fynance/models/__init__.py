af1a19b5f023688d1a104ed09abb7a288d8e20be
ArthurBernard <arthur.bernard.92@gmail.com>
2019-02-18 09:20:51 +0100

    test update __all__

--- fynance/models/__init__.py
+++ fynance/models/__init__.py
@@ -13,4 +13,5 @@ from . import econometric_models_cy
 from .econometric_models_cy import *
 
 __all__ = econometric_models.__all__
+__all__ += ['econometric_models_cy']
 __all__ += econometric_models_cy.__all__
