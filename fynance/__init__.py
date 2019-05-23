abfb3347218c433447fb99acfe25ea51fd78057c
ArthurBernard <arthur.bernard.92@gmail.com>
2019-05-23 18:41:25 +0200

    fix typo

--- fynance/__init__.py
+++ fynance/__init__.py
@@ -1,27 +1,27 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+# coding: utf-8
+# @Author: ArthurBernard
+# @Email: arthur.bernard.92@gmail.com
+# @Date: 2019-02-15 12:50:12
+# @Last modified by: ArthurBernard
+# @Last modified time: 2019-05-23 18:40:41
 
-""" 
+"""
 
-Fynance package is financial tools in python.
+Fynance package is deep learning tools for financial analysis with Python.
 
 .. automodule:: fynance
    :members:
 
 """
 
-from . import models
 from .models import *
-from . import estimator
 from .estimator import *
-from . import tools
 from .tools import *
-from . import neural_networks
 from .neural_networks import *
-from . import backtest
 from .backtest import *
 
-#__version__ = '1.0.4'
+# __version__ = '1.0.4'
 
 __all__ = models.__all__
 __all__ += estimator.__all__
@@ -29,11 +29,11 @@ __all__ += tools.__all__
 __all__ += neural_networks.__all__
 __all__ += backtest.__all__
 
-#__all__ = [
+# __all__ = [
 #    'get_parameters', 'MA', 'ARMA', 'ARMA_GARCH', 'ARMAX_GARCH', # models
 #    'estimator', # not ready
-#    'sharpe', 'roll_sharpe', 'sma', 'wma', 'ema', 'smstd', 'wmstd', 'emstd', 
-#    'z_score', 'rsi', 'bollinger_band', 'hma', 'macd_line', 'signal_line', 
-#    'macd_hist', 'mdd', 'calmar', 'roll_mdd', 'roll_calmar', 'drawdown', # tools 
-#    'RollNeuralNet', # neural_network
-#]
+#    'sharpe', 'roll_sharpe', 'sma', 'wma', 'ema', 'smstd', 'wmstd', 'emstd',
+#    'z_score', 'rsi', 'bollinger_band', 'hma', 'macd_line', 'signal_line',
+#    'macd_hist', 'mdd', 'calmar', 'roll_mdd', 'roll_calmar', 'drawdown',
+#    'RollNeuralNet', # neural_network, # tools,
+# ]
--- fynance/models/__init__.py
+++ fynance/models/__init__.py
@@ -1,19 +1,24 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+# coding: utf-8
+# @Author: ArthurBernard
+# @Email: arthur.bernard.92@gmail.com
+# @Date: 2019-02-18 18:43:15
+# @Last modified by: ArthurBernard
+# @Last modified time: 2019-05-23 18:40:09
 
 """
 
-Some econometric, statistic and financial models.
+Some deep learning, econometric, statistic and/or financial models.
 
 .. automodule:: fynance.models
    :members:
 
 """
 
-from . import econometric_models
 from .econometric_models import *
-from . import econometric_models_cy
 from .econometric_models_cy import *
+from .neural_network import *
 
 __all__ = econometric_models.__all__
 __all__ += econometric_models_cy.__all__
+__all__ += neural_network.__all__
