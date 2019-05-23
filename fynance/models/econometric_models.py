0aab32646afb51831b6abc7f11d0ea65001d3aa6
ArthurBernard <arthur.bernard.92@gmail.com>
2019-05-23 17:06:16 +0200

    fix typo

--- fynance/models/econometric_models.py
+++ fynance/models/econometric_models.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# coding: utf-8
+# @Author: ArthurBernard
+# @Email: arthur.bernard.92@gmail.com
+# @Date: 2019-02-20 10:39:56
+# @Last modified by: ArthurBernard
+# @Last modified time: 2019-05-23 16:45:46
+
+""" Econometric models. """
 
 # Built-in packages
 
@@ -7,21 +14,21 @@
 import numpy as np
 import pandas as pd
 
-# Internal packages
+# Local packages
 from fynance.models.econometric_models_cy import *
 
 __all__ = [
     'get_parameters', 'MA', 'ARMA', 'ARMA_GARCH', 'ARMAX_GARCH'
 ]
 
-#=============================================================================#
+# =========================================================================== #
 #                             PARAMETERS FUNCTION                             #
-#=============================================================================#
+# =========================================================================== #
 
 
 def get_parameters(params, p=0, q=0, Q=0, P=0, cons=True):
-    """ Get parameters for ARMA-GARCH models 
-    
+    """ Get parameters for ARMA-GARCH models.
+
     Parameters
     ----------
     params : np.ndarray[np.float64, ndim=1]
@@ -42,14 +49,14 @@ def get_parameters(params, p=0, q=0, Q=0, P=0, cons=True):
     beta : np.ndarray[np.float64, ndim=1]
         Last part GARCH parameters.
     c : float
-        Constant of ARMA part. 
+        Constant of ARMA part.
     omega : float
         Constants of GARCH part.
 
     See also
     --------
     ARMAX_GARCH, ARMA_GARCH, ARMA and MA.
-    
+
     """
     i = 0
     if cons:
@@ -58,25 +65,25 @@ def get_parameters(params, p=0, q=0, Q=0, P=0, cons=True):
     else:
         c = 0.
     if p > 0:
-        phi = params[i: p+i]
+        phi = params[i: p + i]
         i += p
     else:
         phi = np.array([0.], dtype=np.float64)
     if q > 0:
-        theta = params[i: q+i]
+        theta = params[i: q + i]
         i += q
-    else: 
+    else:
         theta = np.array([0.], dtype=np.float64)
     if Q > 0 or P > 0:
         omega = params[i]
         i += 1
         if Q > 0:
-            alpha = params[i: Q+i]
+            alpha = params[i: Q + i]
             i += Q
         else:
             alpha = np.array([0.], dtype=np.float64)
         if P > 0:
-            beta = params[i: P+i]
+            beta = params[i: P + i]
             i += P
         else:
             beta = np.array([0.], dtype=np.float64)
@@ -87,16 +94,16 @@ def get_parameters(params, p=0, q=0, Q=0, P=0, cons=True):
     return phi, theta, alpha, beta, c, omega
 
 
-#=============================================================================#
+# =========================================================================== #
 #                                   MODELs                                    #
-#=============================================================================#
+# =========================================================================== #
 
 
 def MA(y, theta, c, q):
-    """ Moving Average model of order `q` s.t: 
-    
-    .. math:: y_t = c + \\theta_1 * u_{t-1} + ... + \\theta_q * u_{t-q} + u_t 
-    
+    r""" Moving Average model of order `q` s.t:
+
+    .. math:: y_t = c + \theta_1 * u_{t-1} + ... + \theta_q * u_{t-q} + u_t
+
     Parameters
     ----------
     y : np.ndarray[np.float64, ndim=1]
@@ -107,12 +114,12 @@ def MA(y, theta, c, q):
         Constant of the model.
     q : int
         Order of MA(q) model.
-        
+
     Returns
     -------
     u : np.ndarray[ndim=1, dtype=np.float64]
         Residual of the model.
-    
+
     Examples
     --------
     >>> y = np.array([3, 4, 6, 8, 5, 3])
@@ -125,25 +132,32 @@ def MA(y, theta, c, q):
 
     """
     # Set type of variables
-    if isinstance(y, pd.DataFrame) or isinstance(y, list) or isinstance(y, pd.Series):
+    if isinstance(y, pd.DataFrame) or isinstance(y, pd.Series):
         y = np.asarray(y)
+
+    elif isinstance(y, list):
+        y = np.asarray(y)
+
     y = y.astype(np.float64).reshape([y.size])
+
     if isinstance(theta, list):
         theta = np.asarray(theta)
+
     theta = theta.astype(np.float64).reshape([theta.size])
     # Compute residuals
     u = MA_cy(y, theta, np.float64(c), int(q))
+
     return u
 
 
 def ARMA(y, phi, theta, c, p, q):
-    """ AutoRegressive Moving Average model of order `q` and `p` s.t: 
-    
-    .. math:: 
+    r""" AutoRegressive Moving Average model of order `q` and `p` s.t:
 
-        y_t = c + \\phi_1 * y_{t-1} + ... + \\phi_p * y_{t-p} 
-        + \\theta_1 * u_{t-1} + ... + \\theta_q * u_{t-q} + u_t
-    
+    .. math::
+
+        y_t = c + \phi_1 * y_{t-1} + ... + \phi_p * y_{t-p}
+        + \theta_1 * u_{t-1} + ... + \theta_q * u_{t-q} + u_t
+
     Parameters
     ----------
     y : np.ndarray[np.float64, ndim=1]
@@ -167,34 +181,36 @@ def ARMA(y, phi, theta, c, p, q):
     See also
     --------
     ARMA_GARCH, ARMAX_GARCH and MA.
-    
+
     """
     # Set type variables and parameters
     y = np.asarray(y, dtype=np.float64).reshape([y.size])
     theta = np.asarray(theta, dtype=np.float64)
     phi = np.asarray(phi, dtype=np.float64)
+
     # Compute residuals
     u = ARMA_cy(y, phi, theta, float(c), int(p), int(q))
+
     return u
 
 
 def ARMA_GARCH(y, phi, theta, alpha, beta, c, omega, p, q, Q, P):
-    """ AutoRegressive Moving Average model of order q and p, such that: 
-    
-    .. math:: 
+    r""" AutoRegressive Moving Average model of order q and p, such that:
 
-        y_t = c + \\phi_1 * y_{t-1} + ... + \\phi_p * y_{t-p} 
-        + \\theta_1 * u_{t-1} + ... + \\theta_q * u_{t-q} + u_t
-    
+    .. math::
+
+        y_t = c + \phi_1 * y_{t-1} + ... + \phi_p * y_{t-p}
+        + \theta_1 * u_{t-1} + ... + \theta_q * u_{t-q} + u_t
+
     With Generalized AutoRegressive Conditional Heteroskedasticity volatility
     model of order `Q` and `P`, such that:
-    
-    .. math:: 
+
+    .. math::
         u_t = z_t * h_t
 
-        h_t^2 = \\omega + \\alpha_1 * u^2_{t-1} + ... + \\alpha_Q * u^2_{t-Q}
-        + \\beta_1 * h^2_{t-1} + ... + \\beta_P * h^2_{t-P}
-    
+        h_t^2 = \omega + \alpha_1 * u^2_{t-1} + ... + \alpha_Q * u^2_{t-Q}
+        + \beta_1 * h^2_{t-1} + ... + \beta_P * h^2_{t-P}
+
     Parameters
     ----------
     y : np.ndarray[np.float64, ndim=1]
@@ -223,44 +239,46 @@ def ARMA_GARCH(y, phi, theta, alpha, beta, c, omega, p, q, Q, P):
     Returns
     -------
     u : np.ndarray[np.float64, ndim=1]
-        Residual of the model. 
+        Residual of the model.
     h : np.ndarray[np.float64, ndim=1]
-        Conditional volatility of the model. 
+        Conditional volatility of the model.
 
     See also
     --------
     ARMAX_GARCH, ARMA and MA.
-    
+
     """
     y = np.asarray(y, dtype=np.float64).reshape([y.size])
     theta = np.asarray(theta, dtype=np.float64)
     phi = np.asarray(phi, dtype=np.float64)
     alpha = np.asarray(alpha, dtype=np.float64)
     beta = np.asarray(beta, dtype=np.float64)
+
     u, h = ARMA_GARCH_cy(
-        y, phi, theta, alpha, beta, float(c), float(omega), int(p), int(q), 
+        y, phi, theta, alpha, beta, float(c), float(omega), int(p), int(q),
         int(Q), int(P)
     )
+
     return u, h
 
 
 def ARMAX_GARCH(y, x, phi, psi, theta, alpha, beta, c, omega, p, q, Q, P):
-    """ AutoRegressive Moving Average model of order q and p, such that: 
-    
-    .. math:: 
+    r""" AutoRegressive Moving Average model of order q and p, such that:
 
-        y_t = c + \\phi_1 * y_{t-1} + ... + \\phi_p * y_{t-p} + \\psi_t * x_t 
-        + \\theta_1 * u_{t-1} + ... + \\theta_q * u_{t-q} + u_t
-    
+    .. math::
+
+        y_t = c + \phi_1 * y_{t-1} + ... + \phi_p * y_{t-p} + \psi_t * x_t
+        + \theta_1 * u_{t-1} + ... + \theta_q * u_{t-q} + u_t
+
     With Generalized AutoRegressive Conditional Heteroskedasticity volatility
     model of order `Q` and `P`, such that:
-    
-    .. math:: 
+
+    .. math::
         u_t = z_t * h_t
 
-        h_t^2 = \\omega + \\alpha_1 * u^2_{t-1} + ... + \\alpha_Q * u^2_{t-Q}
-        + \\beta_1 * h^2_{t-1} + ... + \\beta_P * h^2_{t-P}
-    
+        h_t^2 = \omega + \alpha_1 * u^2_{t-1} + ... + \alpha_Q * u^2_{t-Q}
+        + \beta_1 * h^2_{t-1} + ... + \beta_P * h^2_{t-P}
+
     Parameters
     ----------
     y : np.ndarray[np.float64, ndim=1]
@@ -291,14 +309,14 @@ def ARMAX_GARCH(y, x, phi, psi, theta, alpha, beta, c, omega, p, q, Q, P):
     Returns
     -------
     u : np.ndarray[np.float64, ndim=1]
-        Residual of the model. 
+        Residual of the model.
     h : np.ndarray[np.float64, ndim=1]
-        Conditional volatility of the model. 
+        Conditional volatility of the model.
 
     See also
     --------
     ARMA_GARCH, ARMA and MA.
-    
+
     """
     # Set array variables
     y = np.asarray(y, dtype=np.float64).reshape([y.size])
@@ -308,9 +326,11 @@ def ARMAX_GARCH(y, x, phi, psi, theta, alpha, beta, c, omega, p, q, Q, P):
     psi = np.asarray(psi, dtype=np.float64)
     alpha = np.asarray(alpha, dtype=np.float64)
     beta = np.asarray(beta, dtype=np.float64)
+
     # Compute residuals and volatility
     u, h = ARMAX_GARCH_cy(
-        y, x, phi, theta, psi, alpha, beta, float(c), float(omega), int(p), 
+        y, x, phi, theta, psi, alpha, beta, float(c), float(omega), int(p),
         int(q), int(Q), int(P)
     )
+
     return u, h
