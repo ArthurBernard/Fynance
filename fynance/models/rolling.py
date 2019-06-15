80d83dc5596bf610c823241ff8f38cc085355544
ArthurBernard <arthur.bernard.92@gmail.com>
2019-06-15 16:52:51 +0200

    update rolling object

--- fynance/models/rolling.py
+++ fynance/models/rolling.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python3
 # coding: utf-8
+# @Author: ArthurBernard
+# @Email: arthur.bernard.92@gmail.com
+# @Date: 2019-04-23 19:15:17
+# @Last modified by: ArthurBernard
+# @Last modified time: 2019-06-15 16:52:15
+
+""" Basis of rolling models.
+
+Examples
+--------
+# >>> roll_xgb = RollingXGB(X, y)
+# >>> for pred_eval, pred_test in roll_xgb(256, 64):
+# >>>     plot(pred_eval, pred_test)
+
+"""
 
 # Built-in packages
 
 # External packages
 import numpy as np
 
-# Internal packages
+# Local packages
 from fynance.models.xgb import XGBData
 
 
@@ -14,25 +29,28 @@ __all__ = ['RollingBasis']
 
 
 class RollingBasis:
-    """ Basis object to roll a model over a time axis with a train period
-    from `t - n` to `t` and a testing period from `t` to `t + s`.
+    """ Base object to roll a model.
+
+    Rolling over a time axis with a train period from `t - n` to `t` and a
+    testing period from `t` to `t + s`.
 
     Attributes
     ----------
     n, s : int
-        Respectively size of training and testing period
+        Respectively size of training and testing period.
 
     """
-    # TODO : other methods
 
+    # TODO : other methods
     def __init__(self, X, y):
-        """ Initialize shape of target """
+        """ Initialize shape of target. """
         self.T = X.shape[0]
         self.y_shape = y.shape
 
+    # TODO : fix callable method to overwritten problem with torch.nn.Module
     def __call__(self, train_period, test_period, start=0, end=None,
-                 roll_period=None):
-        """ Callable method to set terget features data, and model.
+                 roll_period=None, eval_period=None):
+        """ Callable method to set target features data, and model.
 
         Parameters
         ----------
@@ -45,6 +63,9 @@ class RollingBasis:
         roll_period : int, optional
             Size of the rolling period, default is the same size of the
             testing sub-period.
+        eval_period : int, optional
+            Size of the evaluating period, default is the same size of the
+            testing sub-period if training sub-period is large enough.
 
         Returns
         -------
@@ -64,45 +85,53 @@ class RollingBasis:
         return self
 
     def __iter__(self):
-        """ Set iterative method """
-        self.y_eval = np.zeros([self.y_shape])
-        self.y_test = np.zeros([self.y_shape])
+        """ Set iterative method. """
+        self.y_eval = np.zeros(self.y_shape)
+        self.y_test = np.zeros(self.y_shape)
 
         return self
 
     def __next__(self):
-        """ Incrementing method """
+        """ Incrementing method. """
         # TODO : to finish
+        # Time forward incrementation
         self.t += self.r
 
         if self.t + self.s > self.T:
 
             raise StopIteration
 
-        train_slice = slice(self.t - self.s, self.t)
+        # Set new train and test periods
+        train_slice = slice(self.t - self.n, self.t)
         test_slice = slice(self.t, self.t + self.s)
 
-        self.y_eval[self.t - self.s: self.t] = self.sub_train()
-        self.y_test[self.t: self.t + self.s] = self.sub_predict()
+        # Train model
+        self._train(X=self.X[train_slice], y=self.y[train_slice])
+
+        # Predict on training and testing period
+        self.y_eval[train_slice] = self.sub_predict(self.X[train_slice])
+        self.y_test[test_slice] = self.sub_predict(self.X[test_slice])
 
         return self
 
 
 class RollingXGB(RollingBasis):
-    """ Rolling version of eXtrem Gradient Boosting model. Model will roll
-    train and test periods over a time axis, at time `t` the training period
-    is from `t - n` to `t` and the testing period from `t` to `t + s`.
+    """ Rolling version of eXtrem Gradient Boosting model.
+
+    Model will roll train and test periods over a time axis, at time `t` the
+    training period is from `t - n` to `t` and the testing period from `t` to
+    `t + s`.
 
     Attributes
     ----------
     n, s : int
-        Respectively size of training and testing period
+        Respectively size of training and testing period.
 
     """
-    # TODO : to finish
 
+    # TODO : to finish
     def __init__(self, X, y, **kwargs):
-        """ Setting data to XGBoot model.
+        """ Set data to XGBoot model.
 
         Parameters
         ----------
@@ -119,15 +148,8 @@ class RollingXGB(RollingBasis):
         """
         RollingBasis.__init__(self, X, y)
         self.data = XGBData(X, label=y, **kwargs)
+        self.bst = None
 
-    def sub_train(self):
+    def _train(self):
+        # self.bst = xgb.train(params, )
         pass
-
-"""
-Examples
---------
-
->>> roll_xgb = RollingXGB(X, y)
->>> for pred_eval, pred_test in roll_xgb(256, 64):
->>>     
-"""
