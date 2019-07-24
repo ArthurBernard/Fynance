bab2b8ed6daeb0ae6d2b9d930221965632a0e034
ArthurBernard <arthur.bernard.92@gmail.com>
2019-07-24 16:53:35 +0200

    add MAD and fix typo docstring

--- fynance/tools/metrics.py
+++ fynance/tools/metrics.py
@@ -4,7 +4,7 @@
 # @Email: arthur.bernard.92@gmail.com
 # @Date: 2018-12-14 19:11:40
 # @Last modified by: ArthurBernard
-# @Last modified time: 2019-07-08 13:54:10
+# @Last modified time: 2019-07-24 16:52:59
 
 """ Metric functons used in financial analysis. """
 
@@ -14,8 +14,9 @@
 import numpy as np
 
 # Internal packages
-from fynance.tools.metrics_cy import *
-from fynance.tools.momentums_cy import smstd_cy
+from fynance.tools.metrics_cy import calmar_cy, drawdown_cy, mdd_cy, sharpe_cy
+from fynance.tools.metrics_cy import log_sharpe_cy, roll_mdd_cy
+from fynance.tools.momentums_cy import smstd_cy, sma_cy
 
 # TODO:
 # - Append window size on rolling calmar
@@ -26,14 +27,140 @@ from fynance.tools.momentums_cy import smstd_cy
 
 
 __all__ = [
-    'sharpe', 'mdd', 'calmar', 'roll_sharpe', 'roll_mdd', 'roll_calmar',
-    'drawdown', 'accuracy', 'diversified_ratio',
+    'accuracy', 'calmar', 'diversified_ratio', 'drawdown', 'mad', 'mdd',
+    'roll_calmar', 'roll_mad', 'roll_mdd', 'roll_sharpe', 'sharpe',
 ]
 
 
 # =========================================================================== #
 #                                   Metrics                                   #
 # =========================================================================== #
+
+
+def accuracy(y_true, y_pred, sign=True):
+    """ Compute the accuracy of prediction.
+
+    Parameters
+    ----------
+    y_true : np.ndarray[ndim=1, dtype=np.float64]
+        Vector of true series.
+    y_pred : np.ndarray[ndim=1, dtype=np.float64]
+        Vector of predicted series.
+    sign : bool, optional
+        Check sign accuracy if true, else check exact accuracy, default
+        is True.
+
+    Returns
+    -------
+    float
+        Accuracy of prediction as float between 0 and 1.
+
+    Examples
+    --------
+    >>> y_true = np.array([1., .5, -.5, .8, -.2])
+    >>> y_pred = np.array([.5, .2, -.5, .1, .0])
+    >>> accuracy(y_true, y_pred)
+    0.8
+    >>> accuracy(y_true, y_pred, sign=False)
+    0.2
+
+    See Also
+    --------
+    mdd, calmar, sharpe, drawdown
+
+    """
+    if sign:
+        y_true = np.sign(y_true)
+        y_pred = np.sign(y_pred)
+
+    # Check right answeres
+    R = np.sum(y_true == y_pred)
+
+    # Check wrong answeres
+    W = np.sum(y_true != y_pred)
+
+    return R / (R + W)
+
+
+def calmar(series, period=252):
+    """ Compute the compouned annual return over the Maximum DrawDown.
+
+    Parameters
+    ----------
+    series : np.ndarray[np.float64, ndim=1]
+        Time series (price, performance or index).
+    period : int, optional
+        Number of period per year, default is 252 (trading days).
+
+    Returns
+    -------
+    np.float64
+        Value of Calmar ratio.
+
+    Examples
+    --------
+    Assume a series of monthly prices:
+
+    >>> series = np.array([70, 100, 80, 120, 160, 80])
+    >>> calmar(series, period=12)
+    0.6122448979591835
+
+    See Also
+    --------
+    mdd, drawdown, sharpe, roll_calmar
+
+    """
+    series = np.asarray(series, dtype=np.float64).flatten()
+
+    return calmar_cy(series, period=float(period))
+
+
+def diversified_ratio(series, w=None, std_method='std'):
+    r""" Compute diversification ratio of a portfolio.
+
+    Diversification ratio, denoted D, is defined as the ratio of the
+    portfolio's weighted average volatility to its overll volatility,
+    developed by Choueifaty and Coignard [1]_.
+
+    .. math::
+
+        D(P) = \frac{P' \Sigma}{\sqrt{P'VP}}
+
+    With :math:`\Sigma` vector of asset volatilities, :math:`P` vector of
+    weights of asset of portfolio, and :math:`V` matrix of variance-covariance
+    of these assets.
+
+    Parameters
+    ----------
+    series : np.array[ndim=2, dtype=np.float64] of shape (T, N)
+        Portfolio matrix of N assets and T time periods, each column
+        correspond to one series of prices.
+    w : np.array[ndim=1 or 2, dtype=np.float64] of size N, optional
+        Vector of weights, default is None it means it will equaly weighted.
+    std_method : str, optional /!\ Not yet implemented /!\
+        Method to compute variance vector and covariance matrix.
+
+    Returns
+    -------
+    np.float64
+        Value of diversification ratio of the portfolio.
+
+    References
+    ----------
+    .. [1] tobam.fr/wp-content/uploads/2014/12/TOBAM-JoPM-Maximum-Div-2008.pdf
+
+    """
+    T, N = series.shape
+
+    if w is None:
+        w = np.ones([N, 1]) / N
+    else:
+        w = w.reshape([N, 1])
+
+    sigma = np.std(series, axis=0).reshape([N, 1])
+    V = np.cov(series, rowvar=False, bias=True).reshape([N, N])
+
+    return (w.T @ sigma) / np.sqrt(w.T @ V @ w)
 
 
 def drawdown(series):
@@ -73,6 +200,39 @@ def drawdown(series):
     return drawdown_cy(series)
 
 
+def mad(series):
+    """ Compute the Mean Absolute Deviation.
+
+    Compute the mean of the absolute value of the distance to the mean. _[1]
+
+    Parameters
+    ----------
+    series : np.ndarray[np.float64, ndim=1]
+        Time series (price, performance or index).
+
+    Returns
+    -------
+    np.float64
+        Value of mean absolute deviation.
+
+    References
+    ----------
+    .. [1] https://en.wikipedia.org/wiki/Average_absolute_deviation
+
+    Examples
+    --------
+    >>> series = np.array([70., 100., 90., 110., 150., 80.])
+    >>> mad(series)
+    20.0
+
+    See Also
+    --------
+    roll_mad
+
+    """
+    return np.mean(np.abs(series - np.mean(series)))
+
+
 def mdd(series):
     """ Compute the maximum drwdown.
 
@@ -88,7 +248,7 @@ def mdd(series):
     Returns
     -------
     np.float64
-        Scalar of Maximum DrawDown.
+        Value of Maximum DrawDown.
 
     References
     ----------
@@ -108,78 +268,6 @@ def mdd(series):
     series = np.asarray(series, dtype=np.float64).flatten()
 
     return mdd_cy(series)
-
-
-def calmar(series, period=252):
-    """ Compute the compouned annual return over the Maximum DrawDown.
-
-    Parameters
-    ----------
-    series : np.ndarray[np.float64, ndim=1]
-        Time series (price, performance or index).
-    period : int, optional
-        Number of period per year, default is 252 (trading days).
-
-    Returns
-    -------
-    np.float64
-        Scalar of Calmar ratio.
-
-    Examples
-    --------
-    Assume a series of monthly prices:
-
-    >>> series = np.array([70, 100, 80, 120, 160, 80])
-    >>> calmar(series, period=12)
-    0.6122448979591835
-
-    See Also
-    --------
-    mdd, drawdown, sharpe, roll_calmar
-
-    """
-    series = np.asarray(series, dtype=np.float64).flatten()
-
-    return calmar_cy(series, period=float(period))
-
-
-def sharpe(series, period=252, log=False):
-    """ Compute the total return over the volatility.
-
-    Parameters
-    ----------
-    series : numpy.ndarray(dim=1, dtype=float)
-        Prices of the index.
-    period : int, optional
-        Number of period per year, default is 252 (trading days).
-    log : bool, optional
-        If true compute sharpe with the formula for log-returns, default
-        is False.
-
-    Returns
-    -------
-    np.float64
-        Scalar of Sharpe ratio.
-
-    Examples
-    --------
-    Assume a series of monthly prices:
-
-    >>> series = np.array([70, 100, 80, 120, 160, 80])
-    >>> sharpe(series, period=12)
-    0.22494843872918127
-
-    See Also
-    --------
-    mdd, calmar, drawdown, roll_sharpe
-
-    """
-    series = np.asarray(series, dtype=np.float64).flatten()
-
-    if log:
-        return log_sharpe_cy(series, period=float(period))
-
-    return sharpe_cy(series, period=float(period))
 
 
 def perf_index(series, init_value=100.):
@@ -299,52 +387,43 @@ def perf_strat(underlying, signals=None, log=False, init_value=100.,
     return perf_returns(series, log=log, init_value=init_value)
 
 
-def diversified_ratio(series, w=None, std_method='std'):
-    r""" Compute diversification ratio of a portfolio.
-
-    Diversification ratio, denoted D, is defined as the ratio of the
-    portfolio's weighted average volatility to its overll volatility,
-    developed by Choueifaty and Coignard [1]_.
-
-    .. math::
-
-        D(P) = \frac{P' \Sigma}{\sqrt{P'VP}}
-
-    With :math:`\Sigma` vector of asset volatilities, :math:`P` vector of
-    weights of asset of portfolio, and :math:`V` matrix of variance-covariance
-    of these assets.
+def sharpe(series, period=252, log=False):
+    """ Compute the total return over the volatility.
 
     Parameters
     ----------
-    series : np.array[ndim=2, dtype=np.float64] of shape (T, N)
-        Portfolio matrix of N assets and T time periods, each column
-        correspond to one series of prices.
-    w : np.array[ndim=1 or 2, dtype=np.float64] of size N, optional
-        Vector of weights, default is None it means it will equaly weighted.
-    std_method : str, optional /!\ Not yet implemented /!\
-        Method to compute variance vector and covariance matrix.
+    series : numpy.ndarray(dim=1, dtype=float)
+        Prices of the index.
+    period : int, optional
+        Number of period per year, default is 252 (trading days).
+    log : bool, optional
+        If true compute sharpe with the formula for log-returns, default
+        is False.
 
     Returns
     -------
-    D : np.float64
-        Diversification ratio of the portfolio.
+    np.float64
+        Value of Sharpe ratio.
 
-    References
-    ----------
-    .. [1] tobam.fr/wp-content/uploads/2014/12/TOBAM-JoPM-Maximum-Div-2008.pdf
+    Examples
+    --------
+    Assume a series of monthly prices:
+
+    >>> series = np.array([70, 100, 80, 120, 160, 80])
+    >>> sharpe(series, period=12)
+    0.22494843872918127
+
+    See Also
+    --------
+    mdd, calmar, drawdown, roll_sharpe
 
     """
-    T, N = series.shape
+    series = np.asarray(series, dtype=np.float64).flatten()
 
-    if w is None:
-        w = np.ones([N, 1]) / N
-    else:
-        w = w.reshape([N, 1])
+    if log:
+        return log_sharpe_cy(series, period=float(period))
 
-    sigma = np.std(series, axis=0).reshape([N, 1])
-    V = np.cov(series, rowvar=False, bias=True).reshape([N, N])
-
-    return (w.T @ sigma) / np.sqrt(w.T @ V @ w)
+    return sharpe_cy(series, period=float(period))
 
 
 # =========================================================================== #
@@ -353,46 +432,6 @@ def diversified_ratio(series, w=None, std_method='std'):
 
 # TODO : rolling perf metric
 # TODO : rolling diversified ratio
-
-def roll_mdd(series):
-    """ Compute the rolling maximum drwdown.
-
-    Where drawdown is the measure of the decline from a historical peak in
-    some variable [3]_ (typically the cumulative profit or total open equity
-    of a financial trading strategy).
-
-    Parameters
-    ----------
-    series : np.ndarray[np.float64, ndim=1]
-        Time series (price, performance or index).
-    win : int, optional /! NOT YET WORKING /!
-        Size of the rolling window. If less of two, rolling Max DrawDown is
-        compute on all the past. Default is 0.
-
-    Returns
-    -------
-    np.ndrray[np.float64, ndim=1]
-        Series of rolling Maximum DrawDown.
-
-    References
-    ----------
-    .. [3] https://en.wikipedia.org/wiki/Drawdown_(economics)
-
-    Examples
-    --------
-    >>> series = np.array([70, 100, 80, 120, 160, 80])
-    >>> roll_mdd(series)
-    array([0. , 0. , 0.2, 0.2, 0.2, 0.5])
-
-    See Also
-    --------
-    mdd, roll_calmar, roll_sharpe, drawdown
-
-    """
-    series = np.asarray(series, dtype=np.float64).flatten()
-
-    return roll_mdd_cy(series)
-
 
 def roll_calmar(series, period=252.):
     """ Compute the rolling Calmar ratio.
@@ -447,6 +486,46 @@ def roll_calmar(series, period=252.):
     roll_cal[not_null] = annual_return[not_null] / roll_maxdd[not_null]
 
     return roll_cal
+
+
+def roll_mdd(series):
+    """ Compute the rolling maximum drwdown.
+
+    Where drawdown is the measure of the decline from a historical peak in
+    some variable [3]_ (typically the cumulative profit or total open equity
+    of a financial trading strategy).
+
+    Parameters
+    ----------
+    series : np.ndarray[np.float64, ndim=1]
+        Time series (price, performance or index).
+    win : int, optional /! NOT YET WORKING /!
+        Size of the rolling window. If less of two, rolling Max DrawDown is
+        compute on all the past. Default is 0.
+
+    Returns
+    -------
+    np.ndrray[np.float64, ndim=1]
+        Series of rolling Maximum DrawDown.
+
+    References
+    ----------
+    .. [3] https://en.wikipedia.org/wiki/Drawdown_(economics)
+
+    Examples
+    --------
+    >>> series = np.array([70, 100, 80, 120, 160, 80])
+    >>> roll_mdd(series)
+    array([0. , 0. , 0.2, 0.2, 0.2, 0.5])
+
+    See Also
+    --------
+    mdd, roll_calmar, roll_sharpe, drawdown
+
+    """
+    series = np.asarray(series, dtype=np.float64).flatten()
+
+    return roll_mdd_cy(series)
 
 
 def roll_sharpe(series, period=252, win=0, cap=True):
@@ -525,51 +604,6 @@ def roll_sharpe(series, period=252, win=0, cap=True):
         roll_shar[:win][xtrem_val] = 0.
 
     return roll_shar
-
-
-def accuracy(y_true, y_pred, sign=True):
-    """ Compute the accuracy of prediction.
-
-    Parameters
-    ----------
-    y_true : np.ndarray[ndim=1, dtype=np.float64]
-        Vector of true series.
-    y_pred : np.ndarray[ndim=1, dtype=np.float64]
-        Vector of predicted series.
-    sign : bool, optional
-        Check sign accuracy if true, else check exact accuracy, default
-        is True.
-
-    Returns
-    -------
-    float
-        Accuracy of prediction as float between 0 and 1.
-
-    Examples
-    --------
-    >>> y_true = np.array([1., .5, -.5, .8, -.2])
-    >>> y_pred = np.array([.5, .2, -.5, .1, .0])
-    >>> accuracy(y_true, y_pred)
-    0.8
-    >>> accuracy(y_true, y_pred, sign=False)
-    0.2
-
-    See Also
-    --------
-    mdd, calmar, sharpe, drawdown
-
-    """
-    if sign:
-        y_true = np.sign(y_true)
-        y_pred = np.sign(y_pred)
-
-    # Check right answeres
-    R = np.sum(y_true == y_pred)
-
-    # Check wrong answeres
-    W = np.sum(y_true != y_pred)
-
-    return R / (R + W)
 
 
 if __name__ == '__main__':
