87b9f9a0f9beaffba50479bcce82075ee2f22afb
ArthurBernard <arthur.bernard.92@gmail.com>
2019-09-11 10:00:55 +0200

    update version to 1.0.5

--- setup.py
+++ setup.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# coding: utf-8
+# @Author: ArthurBernard
+# @Email: arthur.bernard.92@gmail.com
+# @Date: 2019-02-19 19:54:59
+# @Last modified by: ArthurBernard
+# @Last modified time: 2019-09-11 09:56:05
+
+""" Setup script. """
+
+# Built-in packages
+import sys
+from setuptools import setup, find_packages
+from distutils.extension import Extension
+from distutils.command.build_ext import build_ext
+
+# Third party packages
+import numpy
 
 # Set this to True to enable building extensions using Cython.
 # Set it to False to build extensions from the C file (that
 # was previously created using Cython).
 USE_CYTHON = 'auto'
-
-import sys
-from setuptools import setup, find_packages
-from distutils.extension import Extension
-from distutils.command.build_ext import build_ext
-import numpy
 
 CLASSIFIERS = [
     'Development Status :: 4 - Beta',
@@ -30,10 +40,10 @@ CLASSIFIERS = [
 
 MAJOR = 1
 MINOR = 0
-PATCH = 4
+PATCH = 5
 VERSION = '{}.{}.{}'.format(MAJOR, MINOR, PATCH)
 
-DESCRIPTION = 'Python and Cython scripts of machine learning, econometrics ' 
+DESCRIPTION = 'Python and Cython scripts of machine learning, econometrics '
 DESCRIPTION += 'and statistical tools for financial analysis [In progress]'
 
 build_requires = [
@@ -54,8 +64,8 @@ if USE_CYTHON or USE_CYTHON == 'auto':
         USE_CYTHON = True
     except ImportError:
         if not USE_CYTHON == 'auto':
-            print("If USE_CYTHON is set to True, Cython is required to", 
-                "compile fynance. Please install Cython or don't set", 
+            print("If USE_CYTHON is set to True, Cython is required to",
+                "compile fynance. Please install Cython or don't set",
                 "USE_CYTHON to True.")
             raise ImportError
         else:
@@ -72,22 +82,22 @@ else:
 
 extensions = [
     Extension(
-        'fynance.models.econometric_models_cy', 
+        'fynance.models.econometric_models_cy',
         ['fynance/models/econometric_models_cy' + ext],
         include_dirs=[numpy.get_include(), '.']
     ),
     Extension(
-        'fynance.tools.metrics_cy', 
+        'fynance.tools.metrics_cy',
         ['fynance/tools/metrics_cy' + ext],
         include_dirs=[numpy.get_include(), '.']
     ),
     Extension(
-        'fynance.tools.momentums_cy', 
+        'fynance.tools.momentums_cy',
         ['fynance/tools/momentums_cy' + ext],
         include_dirs=[numpy.get_include(), '.']
     ),
     Extension(
-        'fynance.estimator.estimator_cy', 
+        'fynance.estimator.estimator_cy',
         ['fynance/estimator/estimator_cy' + ext],
         include_dirs=[numpy.get_include(), '.']
     ),
@@ -111,7 +121,7 @@ setup(
     download_url='https://pypi.org/project/fynance/',
     author=['Arthur Bernard'],
     author_email='arthur.bernard.92@gmail.com',
-    packages=find_packages(), #['fynance'],
+    packages=find_packages(),  # ['fynance'],
     cmdclass=cmdclass,
     ext_modules=ext_modules,
     install_requires=build_requires,
