1465d9047c712d382eec6a302ac7a792fb893ac8
ArthurBernard <arthur.bernard.92@gmail.com>
2019-09-13 17:19:12 +0200

    add IVP, MDP and rolling_allocation functions to allocation algorithms

--- doc/source/algorithms.allocation.rst
+++ doc/source/algorithms.allocation.rst
@@ -7,8 +7,17 @@ Some algorithms of portfolio allocation.
 .. autosummary::
 
    fynance.algorithms.allocation.HRP
+   fynance.algorithms.allocation.IVP
+   fynance.algorithms.allocation.MDP
    fynance.algorithms.allocation.MVP
+   fynance.algorithms.allocation.rolling_allocation
 
 .. autofunction:: HRP
 
+.. autofunction:: IVP
+
+.. autofunction:: MDP
+
 .. autofunction:: MVP
+
+.. autofunction:: rolling_allocation
--- fynance/algorithms/allocation.py
+++ fynance/algorithms/allocation.py
@@ -4,7 +4,7 @@
 # @Email: arthur.bernard.92@gmail.com
 # @Date: 2019-09-12 14:52:08
 # @Last modified by: ArthurBernard
-# @Last modified time: 2019-09-12 20:12:47
+# @Last modified time: 2019-09-13 17:17:51
 
 """ Algorithms of portfolio allocation. """
 
@@ -15,10 +15,15 @@ import numpy as np
 import pandas as pd
 import scipy.cluster.hierarchy as sch
 from scipy.spatial.distance import squareform
+from scipy.optimize import Bounds, LinearConstraint, minimize
 
 # Local packages
+from fynance.tools.metrics import diversified_ratio
+from .rolling import _RollingMechanism
 
-__all__ = ['MVP', 'HRP']
+# TODO : cython
+
+__all__ = ['HRP', 'IVP', 'MDP', 'MVP', 'rolling_allocation']
 
 # =========================================================================== #
 #                     HRP developed by Marcos Lopez de Prado                  #
@@ -144,39 +149,39 @@ def _get_IVP(mat_cov):
 
     Parameters
     ----------
-    mat_cov: pd.DataFrame
-        Matrix covariance.
+    mat_cov : array_like
+        Variance-covariance matrix.
 
     Returns
     -------
-    ivp: pd.DataFrame
+    pd.DataFrame
         Matrix of inverse-variance.
 
     """
     ivp = 1. / np.diag(mat_cov)
-    ivp /= ivp.sum()
+    ivp /= np.sum(ivp)
 
     return ivp
 
 
-def HRP(series, method='single', metric='euclidean'):
-    """ Compute weights following the Hierarchical Risk Parity algorithm.
+def HRP(data, method='single', metric='euclidean'):
+    """ Get weights of the Hierarchical Risk Parity allocation.
 
-    Hierarchical Risk Parity is developed by Marco Lopez de Prado [1]_. First
-    step is clustering, second step is allocating weights.
+    Hierarchical Risk Parity algorithm is developed by Marco Lopez de Prado
+    [1]_. First step is clustering and second step is allocating weights.
 
     Parameters
     ----------
-    series : pd.DataFrame
-        Dataframe of price or return's asset series. Some errors will happen if
-        one or more series are constant.
+    data : array_like
+        Each column is a price or return's asset series. Some errors will
+        happen if one or more series are constant.
     method, metric: str
         Parameters for linkage algorithm, default ``method='single'`` and
         ``metric='euclidean'``.
 
     Returns
     -------
-    pd.Series
+    np.ndarray
         Vecotr of weights computed by HRP algorithm.
 
     References
@@ -184,9 +189,12 @@ def HRP(series, method='single', metric='euclidean'):
     .. [1] https://ssrn.com/abstract=2708678
 
     """
+    if not isinstance(data, pd.DataFrame):
+        data = pd.DataFrame(data)
+
     # Compute covariance and correlation matrix
-    mat_cov = series.cov()
-    mat_corr = series.corr()  # .fillna(0)
+    mat_cov = data.cov()
+    mat_corr = data.corr()  # .fillna(0)
     # Compute distance matrix
     mat_dist = _corr_dist(mat_corr).fillna(0)
     mat_dist_corr = squareform(mat_dist)
@@ -196,15 +204,59 @@ def HRP(series, method='single', metric='euclidean'):
     sortIx = mat_corr.index[sortIx].tolist()
     weights = _get_rec_bisec(mat_cov, sortIx)
 
-    return weights
+    return weights.to_numpy(copy=True).reshape([weights.size, 1])
 
 
 # =========================================================================== #
-#                                    IVP                                      #
+#                        Inverse Variance Portfolio                           #
 # =========================================================================== #
 
 
-def MVP(series, normalize=True):
+def IVP(data, normalize=False):
+    r""" Get weights of the Inverse Variance Portfolio allocation.
+
+    Weights are computed by the inverse of the asset's variance [3]_ such that:
+
+    .. math::
+        w_i = \frac{1}{\sigma_k^2} (\sum_{i} \frac{1}{\sigma_i^2})^{-1}
+
+    With math:`\sigma_i^2` is the variance of asset i.
+
+    Parameters
+    ----------
+    data : array_like
+        Each column is a price or return's asset series.
+    normalize : bool, optional
+        If True normalize the weights such that math:`\sum_{i=1}^{N} w_i = 1`
+        and math:`0 \leq w_i \leq 1`.
+
+    Returns
+    -------
+    np.ndarray
+        Vector of weights computed by the IVP algorithm.
+
+    References
+    ----------
+    .. [3] https://en.wikipedia.org/wiki/Inverse-variance_weighting
+
+    """
+    mat_cov = np.cov(data, rowvar=False)
+    w = _get_IVP(mat_cov)
+
+    if normalize:
+        w = w - np.min(w)
+
+        return w / np.sum(w)
+
+    return w.reshape([mat_cov.shape[0], 1])
+
+
+# =========================================================================== #
+#                         Maximum Variance Portfolio                          #
+# =========================================================================== #
+
+
+def MVP(data, normalize=True):
     r""" Get weights of the Minimum Variance Portfolio [2]_ allocation.
 
     The vector of weights noted math:`w` that minimize the portfolio variance
@@ -212,14 +264,14 @@ def MVP(series, normalize=True):
 
     .. math::
         w = \frac{\Omega^{-1} e}{e' \Omega^{-1} e}
-        with \sum_{i=1}^{N} w_i = 1
+        \text{ with } \sum_{i=1}^{N} w_i = 1
 
     Where math:`\Omega` is the asset's variance-covariance matrix and math:`e`
     is a vector of ones.
 
     Parameters
     ----------
-    series : array_like
+    data : array_like
         Each column is a time-series of price or return's asset.
     normalize : boolean, optional
         If True normalize the weigths such that math:`0 \leq w_i \leq 1` and
@@ -232,15 +284,14 @@ def MVP(series, normalize=True):
 
     References
     ----------
-    .. [2] https://breakingdownfinance.com/finance-topics/modern-portfolio-the\
-    ory/minimum-variance-portfolio/
+    .. [2] https://breakingdownfinance.com/finance-topics/modern-portfolio-theory/minimum-variance-portfolio/
 
     See Also
     --------
     HRP
 
     """
-    mat_cov = np.cov(series, rowvar=False)
+    mat_cov = np.cov(data, rowvar=False)
     # Inverse variance matrix
     try:
         iv = np.linalg.inv(mat_cov)
@@ -257,3 +308,134 @@ def MVP(series, normalize=True):
         return w / np.sum(w)
 
     return w
+
+
+# =========================================================================== #
+#    Maximum Diversification Portfolio developed by Choueifaty and Coignard   #
+# =========================================================================== #
+
+
+def MDP(data, w0=None, up_bound=1., low_bound=0.):
+    """ Get weights of Maximum Diversified Portfolio allocation.
+
+    Parameters
+    ----------
+    data : array_like
+        Each column is a series of price or return's asset.
+    w0 : array_like, optional
+        Initial weights to maximize.
+    up_bound, low_bound : float, optional
+        Respectively maximum and minimum values of weights.
+
+    Returns
+    -------
+    array_like
+        Weights that maximize the diversified ratio of the portfolio.
+
+    References
+    ----------
+    .. [2] tobam.fr/wp-content/uploads/2014/12/TOBAM-JoPM-Maximum-Div-2008.pdf
+
+    """
+    T, N = data.shape
+
+    # Set function to minimze
+    def f_max_divers_weights(w):
+        return - diversified_ratio(data, w=w).flatten()
+
+    # Set inital weights
+    if w0 is None:
+        w0 = np.ones([N]) / N
+
+    # Set constraints and minimze
+    const_sum = LinearConstraint(np.ones([1, N]), [1], [1])
+    const_ind = Bounds(low_bound * np.ones([N]), up_bound * np.ones([N]))
+    result = minimize(
+        f_max_divers_weights,
+        w0,
+        method='SLSQP',
+        constraints=[const_sum],
+        bounds=const_ind
+    )
+
+    return result.x.reshape([N, 1])
+
+
+# =========================================================================== #
+#                             Rolling allocation                              #
+# =========================================================================== #
+
+
+def _perf_alloc(data, w, drift=True):
+    if w.ndim == 1 and not isinstance(w, pd.Series):
+        w = w.reshape([w.size, 1])
+
+    if drift:
+        return (data / data[0, :]) @ w
+
+    perf = np.zeros(data.shape)
+    perf[1:] = (data[1:] / data[:-1] - 1)
+
+    return np.cumprod(perf @ w + 1)
+
+
+def rolling_allocation(f, data, n=252, s=63, ret=True, drift=True, **kwargs):
+    """ Roll an algorithm of allocation.
+
+    Parameters
+    ----------
+    f : callable
+        Allocation algorithm that take as parameters a subarray of ``data``
+        and ``**kwargs``, and return a vector (as ``np.ndarray``) of weights.
+    data : array_like
+        Each columns is a series of prices, indexes or performances.
+    n, s : int
+        Respectively the number of observations to compute weights and the
+        number of observations to roll. Default is ``n=252`` and ``s=63``.
+    ret : bool, optional
+        If True (default) pass to ``f`` the returns of ``data``. Otherwise pass
+        ``data`` to ``f``.
+    drift : bool, optional
+        If False performance of the portfolio is computed as if we rebalance
+        the weights of asset at each timeframe. Otherwise we let to drift the
+        weights. Default is True.
+    **kwargs
+        Any keyword arguments to pass to ``f``.
+
+    Returns
+    -------
+    pd.Series
+        Performance of the portfolio allocated following ``f`` algorithm.
+    pd.DataFrame
+        Weights of the portfolio allocated following ``f`` algorithm.
+
+    """
+    data = pd.DataFrame(data)
+    idx = data.index
+    w_mat = pd.DataFrame(index=idx, columns=data.columns)
+    portfolio = pd.Series(100., index=idx, name='portfolio')
+    T = idx.size
+
+    if ret:
+        data_ = data.pct_change()
+
+    else:
+        data_ = data
+
+    roll = _RollingMechanism(idx, n=n, s=s)
+
+    for slice_n, slice_s in roll():
+        # Select data
+        sub_data = data_.loc[slice_n].copy()
+        sub_data = sub_data.dropna(axis=1, how='all').fillna(method='bfill')
+        assets = sub_data.columns
+        # Compute weights
+        w = f(sub_data.values, **kwargs)
+        w_mat.loc[roll.d, assets] = w.flatten()
+        # Compute portfolio performance
+        perf = _perf_alloc(data.loc[slice_s, assets].values, w=w, drift=drift)
+        portfolio.loc[slice_s] = portfolio.loc[roll.d] * perf.flatten()
+
+    w_mat = w_mat.fillna(method='ffill').fillna(0.)
+
+    return portfolio, w_mat
