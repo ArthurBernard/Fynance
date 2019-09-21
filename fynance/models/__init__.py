cdf7af05e0fe9480b2ca96fc541490b3845db148
ArthurBernard <arthur.bernard.92@gmail.com>
2019-09-21 19:58:15 +0200

    add rolling.__all__

--- fynance/models/__init__.py
+++ fynance/models/__init__.py
@@ -4,7 +4,7 @@
 # @Email: arthur.bernard.92@gmail.com
 # @Date: 2019-02-18 18:43:15
 # @Last modified by: ArthurBernard
-# @Last modified time: 2019-05-23 18:40:09
+# @Last modified time: 2019-09-21 16:47:28
 
 """
 
@@ -18,7 +18,9 @@ Some deep learning, econometric, statistic and/or financial models.
 from .econometric_models import *
 from .econometric_models_cy import *
 from .neural_network import *
+from .rolling import *
 
 __all__ = econometric_models.__all__
 __all__ += econometric_models_cy.__all__
 __all__ += neural_network.__all__
+__all__ += rolling.__all__
