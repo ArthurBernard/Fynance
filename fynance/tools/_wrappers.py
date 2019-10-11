49f4d14e11e9b1051f7ec1bc9795782613e2d2b9
ArthurBernard <arthur.bernard.92@gmail.com>
2019-10-11 18:13:53 +0200

    add wrapper function to check dtype of array

---
+++ fynance/tools/_wrappers.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# @Author: ArthurBernard
+# @Email: arthur.bernard.92@gmail.com
+# @Date: 2019-10-11 10:10:43
+# @Last modified by: ArthurBernard
+# @Last modified time: 2019-10-11 18:01:36
+
+""" Some wrappers functions. """
+
+# Built-in packages
+
+# Third party packages
+import numpy as np
+
+# Local packages
+
+
+def wrap_dtype(func):
+    """ Check the dtype of the x array.
+
+    Convert dtype of x to np.float64 before to pass to cython function and
+    convert to specified dtype at the end.
+
+    """
+    def check_dtype(x, *args, dtype=None, **kwargs):
+        if dtype is None:
+            dtype = x.dtype
+
+        if x.dtype != np.float64:
+            x = x.astype(np.float64)
+
+        if dtype != np.float64:
+            return func(x, *args, dtype=dtype, **kwargs).astype(dtype)
+
+        return func(x, *args, dtype=dtype, **kwargs)
+
+    return check_dtype
