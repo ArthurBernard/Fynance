63427f346c09b39fcbd8f0b3476c09e077b3d754
ArthurBernard <arthur.bernard.92@gmail.com>
2019-10-16 13:19:03 +0200

    add dtype and axis parameters, allow two-dimensional array

--- fynance/tools/momentums.py
+++ fynance/tools/momentums.py
@@ -4,18 +4,18 @@
 # @Email: arthur.bernard.92@gmail.com
 # @Date: 2019-02-20 19:57:13
 # @Last modified by: ArthurBernard
-# @Last modified time: 2019-09-11 09:57:53
+# @Last modified time: 2019-10-16 11:13:30
 
 """ Statical momentum functions. """
 
 # Built-in packages
-import warnings
 
 # External packages
 import numpy as np
 
 # Local packages
-from .momentums_cy import *
+from fynance.tools.momentums_cy import *
+from fynance.tools._wrappers import WrapperArray
 
 __all__ = [
     'sma', 'wma', 'ema', 'smstd', 'wmstd', 'emstd',
@@ -25,147 +25,187 @@ __all__ = [
 #        - Momentums of order 4
 #        - Momentums of order k
 
-
-def _momentum_process(series, lags):
-    shape = series.shape
-    # Check lags
-    if shape[0] < lags:
-        lags = shape[0]
-        warnings.warn('Lags parameters is greater than size of series.')
-
-    if not isinstance(lags, int):
-        lags = int(lags)
-
-    # Check type of series
-    if not isinstance(series.dtype, np.float64):
-        series = series.astype(np.float64)
-
-    # Check shape of series
-    if len(shape) > 1:
-        series = series.flatten()
-
-    return series, lags
-
-
 # =========================================================================== #
 #                               Moving Averages                               #
 # =========================================================================== #
 
 
-def sma(series, lags=21):
-    r""" Compute simple moving average along k lags.
+@WrapperArray('dtype', 'axis', 'lags')
+def sma(X, k, axis=0, dtype=None):
+    r""" Compute simple moving average(s) for `k` lags for each `X`' series.
 
-    .. math:: sma_t = \frac{1}{k} \sum^{k-1}_{i=0} series_{t-i}
+    .. math:: sma_t = \\frac{1}{k} \\sum^{k-1}_{i=0} X_{t-i}
 
     Parameters
     ----------
-    series : np.ndarray[dtype=np.float64, ndim=1]
-        Index or returns.
-    lags : int, optional
-        Number of lags for ma, default is 21.
+    X : np.ndarray[dtype, ndim=1 or 2]
+        Elements to compute the moving average.
+    k : int
+        Number of lags used for computation, must be strictly positive.
+    axis : {0, 1}, optional
+        Axis along wich the computation is done. Default is 0.
+    dtype : np.dtype, optional
+        The type of the output array.  If `dtype` is not given, infer the data
+        type from `X` input.
 
     Returns
     -------
-    np.ndarray[dtype=np.float64, ndim=1]
-        Vector of moving average of series.
+    np.ndarray[dtype, ndim=1 or 2]
+        Simple moving average of each series.
 
     Examples
     --------
-    >>> series = np.array([60, 100, 80, 120, 160, 80])
-    >>> sma(series, lags=3)
+    >>> X = np.array([60, 100, 80, 120, 160, 80])
+    >>> sma(X, k=3, dtype=np.float64, axis=0)
     array([ 60.,  80.,  80., 100., 120., 120.])
+    >>> X = np.array([[60, 60], [100, 100], [80, 80],
+    ...               [120, 120], [160, 160], [80, 80]])
+    >>> sma(X, k=3, dtype=np.float64, axis=0)
+    array([[ 60.,  60.],
+           [ 80.,  80.],
+           [ 80.,  80.],
+           [100., 100.],
+           [120., 120.],
+           [120., 120.]])
+    >>> sma(X, k=3, dtype=np.float64, axis=1)
+    array([[ 60.,  60.],
+           [100., 100.],
+           [ 80.,  80.],
+           [120., 120.],
+           [160., 160.],
+           [ 80.,  80.]])
+
 
     See Also
     --------
-    wma, ema
+    wma, ema, smstd
 
     """
-    series, lags = _momentum_process(series, lags=lags)
-
-    return sma_cy(series, lags=lags)
-    # return sma_cy(series.flatten().astype(np.float64), lags=int(lags))
+    return _sma(X, k, dtype=dtype, axis=axis)
 
 
-def wma(series, lags=21):
-    r""" Weighted moving average along k lags.
+def _sma(X, k, axis=0, dtype=None):
+    if len(X.shape) == 2:
+
+        return np.asarray(sma_cy_2d(X, k))
+
+    return np.asarray(sma_cy_1d(X, k))
+
+
+@WrapperArray('dtype', 'axis', 'lags')
+def wma(X, k, axis=0, dtype=None):
+    r""" Compute weighted moving average(s) for `k` lags for each `X`' series.
 
     .. math::
-        wma_t = \frac{2}{k (k-1)} \sum^{k-1}_{i=0} (k-i) \times series_{t-i}
+        wma_t = \frac{2}{k (k-1)} \sum^{k-1}_{i=0} (k-i) \times X_{t-i}
 
     Parameters
     ----------
-    series : np.ndarray[dtype=np.float64, ndim=1]
-        Index or returns.
-    lags : int, optional
-        Number of lags for ma, default is 21.
+    X : np.ndarray[dtype, ndim=1 or 2]
+        Elements to compute the moving average.
+    k : int
+        Number of lags used for computation, must be strictly positive.
+    axis : {0, 1}, optional
+        Axis along wich the computation is done. Default is 0.
+    dtype : np.dtype, optional
+        The type of the output array.  If `dtype` is not given, infer the data
+        type from `X` input.
 
     Returns
     -------
-    np.ndarray[dtype=np.float64, ndim=1]
-        Moving average of series.
+    np.ndarray[dtype, ndim=1 or 2]
+        Weighted moving average of each series.
 
     Examples
     --------
-    >>> series = np.array([60, 100, 80, 120, 160, 80])
-    >>> wma(series, lags=3)
+    >>> X = np.array([60, 100, 80, 120, 160, 80])
+    >>> wma(X, 3, dtype=np.float64)
+    array([ 60.        ,  86.66666667,  83.33333333, 103.33333333,
+           133.33333333, 113.33333333])
+    >>> X = X.reshape([6, 1])
+    >>> wma(X, 3, dtype=np.float64).flatten()
     array([ 60.        ,  86.66666667,  83.33333333, 103.33333333,
            133.33333333, 113.33333333])
 
     See Also
     --------
-    sma, ema
+    sma, ema, wmstd
 
     """
-    series, lags = _momentum_process(series, lags=lags)
-
-    return wma_cy(series, lags=lags)
-    # return wma_cy(series.flatten().astype(np.float64), lags=int(lags))
+    return _wma(X, k, axis=axis, dtype=dtype)
 
 
-def ema(series, alpha=0.94, lags=None):
-    r""" Exponential moving average along k lags.
+def _wma(X, k, axis=0, dtype=None):
+    if len(X.shape) == 2:
+
+        return np.asarray(wma_cy_2d(X, k))
+
+    return np.asarray(wma_cy_1d(X, k))
+
+
+@WrapperArray('dtype', 'axis')
+def ema(X, alpha=0.94, k=None, axis=0, dtype=None):
+    r""" Compute exponential moving average(s) for each `X`' series.
 
     .. math::
-        ema_t = \alpha \times ema_{t-1} + (1-\alpha) \times series_t
+        ema_t = \alpha \times ema_{t-1} + (1-\alpha) \times X_t
 
     Parameters
     ----------
-    series : np.ndarray[dtype=np.float64, ndim=1]
-        Index or returns.
+    X : np.ndarray[dtype, ndim=1 or 2]
+        Elements to compute the moving average.
     alpha : float, optional
         Multiplier, default is 0.94 corresponding at 20 lags memory.
-    lags : int, optional
-        Number of days.
+    k : int, optional
+        Number of lags used for computation, must be positive. Default is None.
+    axis : {0, 1}, optional
+        Axis along wich the computation is done. Default is 0.
+    dtype : np.dtype, optional
+        The type of the output array.  If `dtype` is not given, infer the data
+        type from `X` input.
 
     Returns
     -------
-    np.ndarray[dtype=np.float64, ndim=1]
-        Moving average of series.
+    np.ndarray[dtype, ndim=1 or 2]
+        Exponential moving average of each series.
 
     Examples
     --------
-    >>> series = np.array([60, 100, 80, 120, 160, 80])
-    >>> ema(series, lags=3)
+    >>> X = np.array([60, 100, 80, 120, 160, 80])
+    >>> ema(X, k=3, dtype=np.float64)
     array([ 60.,  80.,  80., 100., 130., 105.])
-    >>> ema(series, alpha=0.5)
+    >>> ema(X, alpha=0.5, dtype=np.float64)
     array([ 60.,  80.,  80., 100., 130., 105.])
 
     Notes
     -----
-    If `lags` is specified :math:`\alpha = 1 - \frac{2}{1 + k}`
+    If `k` lags is specified :math:`\alpha` is overwritten by
+    :math:`\alpha = 1 - \frac{2}{1 + k}`
 
     See Also
     --------
-    sma, wma
+    sma, wma, emstd
 
     """
-    if lags is not None:
-        alpha = 1 - 2 / (1 + lags)
+    if k is None:
+        pass
 
-    series, _ = _momentum_process(series, lags=series.size)
+    elif k <= 0:
 
-    return ema_cy(series, alpha=float(alpha))
-    # return ema_cy(series.flatten().astype(np.float64), alpha=float(alpha))
+        raise ValueError('lag {} must be greater than 0.'.format(k))
+
+    else:
+        alpha = 1 - 2 / (1 + k)
+
+    return _ema(X, alpha, axis=axis, dtype=dtype)
+
+
+def _ema(X, alpha=0.94, axis=0, dtype=None):
+    if len(X.shape) == 2:
+
+        return np.asarray(ema_cy_2d(X, float(alpha)))
+
+    return np.asarray(ema_cy_1d(X, float(alpha)))
 
 
 # =========================================================================== #
@@ -173,115 +213,174 @@ def ema(series, alpha=0.94, lags=None):
 # =========================================================================== #
 
 
-def smstd(series, lags=21):
-    r""" Compute simple moving standard deviation along k lags.
+@WrapperArray('dtype', 'axis', 'lags')
+def smstd(X, k, axis=0, dtype=None):
+    r""" Compute simple moving standard deviation(s) for each `X`' series'.
 
     .. math::
-        smstd_t = \sqrt{\frac{1}{k} \sum^{k-1}_{i=0} (p_{t-i} - sma_t)^2}
+        smstd_t = \sqrt{\frac{1}{k} \sum^{k-1}_{i=0} (X_{t-i} - sma_t)^2}
 
     Parameters
     ----------
-    series : np.ndarray[dtype=np.float64, ndim=1]
-        Index or returns.
-    lags : int, optional
-        Number of lags for ma, default is 21.
+    X : np.ndarray[dtype, ndim=1 or 2]
+        Elements to compute the moving standard deviation.
+    k : int
+        Number of lags used for computation, must be strictly positive.
+    axis : {0, 1}, optional
+        Axis along wich the computation is done. Default is 0.
+    dtype : np.dtype, optional
+        The type of the output array.  If `dtype` is not given, infer the data
+        type from `X` input.
 
     Returns
     -------
-    np.ndarray[dtype=np.float64, ndim=1]
-        Moving standard deviation of series.
+    np.ndarray[dtype, ndim=1 or 2]
+        Simple moving standard deviation of each series.
 
     Examples
     --------
-    >>> series = np.array([60, 100, 80, 120, 160, 80])
-    >>> smstd(series, lags=3)
+    >>> X = np.array([60, 100, 80, 120, 160, 80])
+    >>> smstd(X, 3, dtype=np.float64)
+    array([ 0.        , 20.        , 16.32993162, 16.32993162, 32.65986324,
+           32.65986324])
+    >>> smstd(X.reshape([6, 1]), 3, dtype=np.float64).flatten()
     array([ 0.        , 20.        , 16.32993162, 16.32993162, 32.65986324,
            32.65986324])
 
     See Also
     --------
-    wmstd, emstd
+    sma, wmstd, emstd
 
     """
-    return smstd_cy(series.flatten().astype(np.float64), lags=int(lags))
+    return _smstd(X, k, axis=axis, dtype=dtype)
 
 
-def wmstd(series, lags=21):
-    r""" Weighted moving standard deviation along k lags.
+def _smstd(X, k, axis=0, dtype=None):
+    if len(X.shape) == 2:
+
+        return np.asarray(smstd_cy_2d(X, k))
+
+    return np.asarray(smstd_cy_1d(X, k))
+
+
+@WrapperArray('dtype', 'axis', 'lags')
+def wmstd(X, k, axis=0, dtype=None):
+    r""" Compute weighted moving standard(s) deviation for each `X`' series'.
 
     .. math::
-        wma_t = \frac{2}{k (k-1)} \sum^{k-1}_{i=0} (k-i) \times series_{t-i}
+        wma_t = \frac{2}{k (k-1)} \sum^{k-1}_{i=0} (k-i) \times X_{t-i}
 
         wmstd_t = \sqrt{\frac{2}{k(k-1)} \sum^{k-1}_{i=0}
-        (k-i) \times (series_{t-i} - wma_t)^2}
+        (k-i) \times (X_{t-i} - wma_t)^2}
 
     Parameters
     ----------
-    series : np.ndarray[dtype=np.float64, ndim=1]
-        Series of prices, index or returns.
-    lags : int, optional
-        Number of days, default is 21.
+    X : np.ndarray[dtype, ndim=1 or 2]
+        Elements to compute the moving standard deviation.
+    k : int
+        Number of lags used for computation, must be strictly positive.
+    axis : {0, 1}, optional
+        Axis along wich the computation is done. Default is 0.
+    dtype : np.dtype, optional
+        The type of the output array.  If `dtype` is not given, infer the data
+        type from `X` input.
 
     Returns
     -------
-    np.ndarray[dtype=np.float64, ndim=1]
-        Moving standard deviation of series.
+    np.ndarray[dtype, ndim=1 or 2]
+        Weighted moving standard deviation of each series.
 
     Examples
     --------
-    >>> series = np.array([60, 100, 80, 120, 160, 80])
-    >>> wmstd(series, lags=3)
+    >>> X = np.array([60, 100, 80, 120, 160, 80])
+    >>> wmstd(X, 3, dtype=np.float64)
+    array([ 0.        , 18.85618083, 13.74368542, 17.95054936, 29.8142397 ,
+           35.90109871])
+    >>> wmstd(X.reshape([6, 1]), 3, dtype=np.float64).flatten()
     array([ 0.        , 18.85618083, 13.74368542, 17.95054936, 29.8142397 ,
            35.90109871])
 
     See Also
     --------
-    smstd, emstd
+    wma, smstd, emstd
 
     """
-    return wmstd_cy(series.flatten().astype(np.float64), lags=int(lags))
+    return _wmstd(X, k, axis=axis, dtype=dtype)
 
 
-def emstd(series, alpha=0.94, lags=None):
-    r""" Exponential moving standard deviation along k lags.
+def _wmstd(X, k, axis=0, dtype=None):
+    if len(X.shape) == 2:
+
+        return np.asarray(wmstd_cy_2d(X, k))
+
+    return np.asarray(wmstd_cy_1d(X, k))
+
+
+@WrapperArray('dtype', 'axis')
+def emstd(X, alpha=0.94, k=None, axis=0, dtype=None):
+    r""" Compute exponential moving standard deviation(s) for each `X`' series.
 
     .. math::
-        emstd_t = \sqrt{\alpha\times emstd_{t-1}^2+(1-\alpha)\times series_t^2}
+        emstd_t = \sqrt{\alpha\times emstd_{t-1}^2+(1-\alpha)\times X_t^2}
 
     Parameters
     ----------
-    series : np.ndarray[dtype=np.float64, ndim=1]
-        Index or returns.
+    X : np.ndarray[dtype, ndim=1 or 2]
+        Elements to compute the moving standard deviation.
     alpha : float, optional
         Multiplier, default is 0.94 corresponding at 20 lags memory.
-    lags : int, optional
-        Number of days.
+    k : int, optional
+        Number of lags used for computation, must be positive. Default is None.
+    axis : {0, 1}, optional
+        Axis along wich the computation is done. Default is 0.
+    dtype : np.dtype, optional
+        The type of the output array.  If `dtype` is not given, infer the data
+        type from `X` input.
 
     Returns
     -------
-    np.ndarray[dtype=np.float64, ndim=1]
-        Moving standard deviation of series.
+    np.ndarray[dtype, ndim=1 or 2]
+        Exponential moving standard deviation of each series.
 
     Examples
     --------
-    >>> series = np.array([60, 100, 80, 120, 160, 80])
-    >>> emstd(series, lags=3)
-    array([ 0.        , 28.28427125, 20.        , 31.6227766 , 47.95831523,
-           48.98979486])
+    >>> X = np.array([60, 100, 80, 120, 160, 80])
+    >>> emstd(X, k=3, dtype=np.float64)
+    array([ 0.        , 14.14213562, 10.        , 15.8113883 , 23.97915762,
+           24.49489743])
+    >>> emstd(X.reshape([6, 1]), k=3, dtype=np.float64).flatten()
+    array([ 0.        , 14.14213562, 10.        , 15.8113883 , 23.97915762,
+           24.49489743])
 
     Notes
     -----
-    If `lags` is specified :math:`\alpha = 1 - \frac{2}{1 + k}`
+    If `k` lags is specified :math:`\alpha` is overwritten by
+    :math:`\alpha = 1 - \frac{2}{1 + k}`
 
     See Also
     --------
-    smstd, wmstd
+    ema, smstd, wmstd
 
     """
-    if lags is not None:
-        alpha = 1 - 2 / (1 + lags)
+    if k is None:
+        pass
 
-    return emstd_cy(series.flatten().astype(np.float64), alpha=float(alpha))
+    elif k <= 0:
+
+        raise ValueError('lag {} must be greater than 0.'.format(k))
+
+    else:
+        alpha = 1 - 2 / (1 + k)
+
+    return _emstd(X, alpha, axis=axis, dtype=dtype)
+
+
+def _emstd(X, alpha=0.94, axis=0, dtype=None):
+    if len(X.shape) == 2:
+
+        return np.asarray(emstd_cy_2d(X, float(alpha)))
+
+    return np.asarray(emstd_cy_1d(X, float(alpha)))
 
 
 if __name__ == '__main__':
