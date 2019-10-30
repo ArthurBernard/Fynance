3a54f1a163e849d32c5358f60db96eddca3774d8
ArthurBernard <arthur.bernard.92@gmail.com>
2019-10-30 19:33:19 +0100

    add tests on rolling functions

--- fynance/tests/tools/test_metrics.py
+++ fynance/tests/tools/test_metrics.py
@@ -4,7 +4,7 @@
 # @Email: arthur.bernard.92@gmail.com
 # @Date: 2019-10-23 12:31:27
 # @Last modified by: ArthurBernard
-# @Last modified time: 2019-10-29 22:00:36
+# @Last modified time: 2019-10-30 19:30:03
 
 """ Test metric functions. """
 
@@ -168,13 +168,13 @@ def test_perf_strat():
 def test_sharpe(set_variables):
     x_1d, x_2d = set_variables
     f = fy.sharpe
-    #a_1d = f(x_1d, dtype=np.float32)
-    #a_2d = f(x_2d, dtype=np.float32)
+    a_1d = f(x_1d, dtype=np.float32)
+    a_2d = f(x_2d, dtype=np.float32)
 
-    #assert a_1d.dtype == np.float32
-    #assert (a_1d == a_2d.flatten()).all()
-    #assert a_1d.shape == ()
-    #assert a_2d.shape == (1,)
+    assert a_1d.dtype == np.float32
+    assert (a_1d == a_2d.flatten()).all()
+    assert a_1d.shape == ()
+    assert a_2d.shape == (1,)
 
     with pytest.raises(ArraySizeError) as execinfo:
         f(x_2d, axis=1)
@@ -184,8 +184,8 @@ def test_sharpe(set_variables):
         f(x_2d, axis=0, ddof=7)
     execinfo.match(r'7.*6')
 
-    res = np.array([2.8867514, -0.4330127], dtype=np.float32)
-    #assert (f(x_2d.reshape([3, 2]), period=3, dtype=np.float32) == res).all()
+    res = np.array([2.3145502, -0.52489066], dtype=np.float32)
+    assert (f(x_2d.reshape([3, 2]), period=3, dtype=np.float32) == res).all()
 
 
 def test_zscore():
@@ -196,15 +196,145 @@ def test_roll_annual_return(set_variables):
     x_1d, x_2d = set_variables
     f = fy.annual_return
     roll_f = fy.roll_annual_return
-    a_1d = roll_f(x_1d, period=12, dtype=np.float32, c=True)
+    a_1d = roll_f(x_1d, period=12, dtype=np.float32)
+    a_2d = roll_f(x_2d, period=12, dtype=np.float32)
+
+    assert a_1d.dtype == np.float32
+    assert (a_1d == a_2d.flatten()).all()
+    assert a_1d.shape == (6,)
+    assert a_2d.shape == (6, 1)
+
+    a_1d_w3 = roll_f(x_1d, period=12, w=3, dtype=np.float32)
+    a_1d_d2 = roll_f(x_1d, period=12, ddof=2, dtype=np.float32)
     for t in range(1, x_1d.size):
         assert a_1d[t] == f(x_1d[: t + 1], period=12, dtype=np.float32)
+        assert a_1d_w3[t] == f(x_1d[max(0, t - 3 + 1): t + 1], period=12,
+                               dtype=np.float32)
+        if t >= 2:
+            assert a_1d_d2[t] == f(x_1d[: t + 1], ddof=2, period=12,
+                                   dtype=np.float32)
+
+    with pytest.raises(ValueError) as execinfo:
+        roll_f(x_1d, period=12, w=1, dtype=np.float32)
+    execinfo.match(r'1.*2')
+
+    with pytest.raises(ValueError) as execinfo:
+        roll_f(x_1d, period=12, w=2, ddof=2, dtype=np.float32)
+    execinfo.match(r'w=2.*ddof=2')
+
+    with pytest.raises(ValueError) as execinfo:
+        roll_f(x_1d, period=12, w=2, ddof=3, dtype=np.float32)
+    execinfo.match(r'w=2.*ddof=3')
 
 
 def test_roll_volatility_return(set_variables):
     x_1d, x_2d = set_variables
     f = fy.annual_volatility
     roll_f = fy.roll_annual_volatility
-    a_1d = roll_f(x_1d, period=12, dtype=np.float32, c=True)
+    a_1d = roll_f(x_1d, period=12, log=False, dtype=np.float32)
+    a_2d = roll_f(x_2d, period=12, log=False, dtype=np.float32)
+
+    assert a_1d.dtype == np.float32
+    assert (a_1d == a_2d.flatten()).all()
+    assert a_1d.shape == (6,)
+    assert a_2d.shape == (6, 1)
+
+    a_1d_log = roll_f(x_1d, period=12, log=True, dtype=np.float32)
+    a_1d_w3 = roll_f(x_1d, period=12, w=3, log=True, dtype=np.float32)
+    a_1d_dof = roll_f(x_1d, period=12, ddof=2, dtype=np.float32)
+    for t in range(1, x_1d.size):
+        assert a_1d[t] == f(x_1d[: t + 1], period=12, log=False,
+                            dtype=np.float32)
+        assert a_1d_log[t] == f(x_1d[: t + 1], period=12, log=True,
+                                dtype=np.float32)
+        #assert a_1d_w3[t] == f(x_1d[max(0, t - 3 + 1): t + 1], log=True,
+        #                       period=12, dtype=np.float32)
+        if t >= 2:
+            assert a_1d_dof[t] == f(x_1d[: t + 1], period=12, ddof=2,
+                                    dtype=np.float32)
+
+    with pytest.raises(ValueError) as execinfo:
+        roll_f(x_1d, period=12, w=1, dtype=np.float32)
+    execinfo.match(r'1.*3')
+
+    with pytest.raises(ValueError) as execinfo:
+        roll_f(x_1d, period=12, w=3, ddof=3, dtype=np.float32)
+    execinfo.match(r'w=3.*ddof=3')
+
+    with pytest.raises(ValueError) as execinfo:
+        roll_f(x_1d, period=12, w=3, ddof=4, dtype=np.float32)
+    execinfo.match(r'w=3.*ddof=4')
+
+
+def test_roll_drawdown(set_variables):
+    x_1d, x_2d = set_variables
+    f = fy.drawdown
+    roll_f = fy.roll_drawdown
+    a_1d = roll_f(x_1d, dtype=np.float32)
+    a_2d = roll_f(x_2d, dtype=np.float32)
+
+    assert a_1d.dtype == np.float32
+    assert (a_1d == a_2d.flatten()).all()
+    assert a_1d.shape == (6,)
+    assert a_2d.shape == (6, 1)
+
+    a_1d_w3 = roll_f(x_1d, w=3, dtype=np.float32)
+    a_1d_raw = roll_f(x_1d, raw=True, dtype=np.float32)
+    for t in range(1, x_1d.size):
+        assert a_1d[t] == f(x_1d[: t + 1], dtype=np.float32)[t]
+        assert a_1d_w3[t] == f(x_1d[max(0, t - 3 + 1): t + 1], dtype=np.float32)[-1]
+        if t >= 2:
+            assert a_1d_raw[t] == f(x_1d[: t + 1], raw=True, dtype=np.float32)[t]
+
+
+def test_roll_calmar(set_variables):
+    x_1d, x_2d = set_variables
+    f = fy.calmar
+    roll_f = fy.roll_calmar
+    a_1d = roll_f(x_1d, period=12, dtype=np.float32)
+    a_2d = roll_f(x_2d, period=12, dtype=np.float32)
+
+    assert a_1d.dtype == np.float32
+    assert (a_1d == a_2d.flatten()).all()
+    assert a_1d.shape == (6,)
+    assert a_2d.shape == (6, 1)
+
+    a_1d_w3 = roll_f(x_1d, period=12, w=3, dtype=np.float32)
+    a_1d_d2 = roll_f(x_1d, period=12, ddof=2, dtype=np.float32)
     for t in range(1, x_1d.size):
         assert a_1d[t] == f(x_1d[: t + 1], period=12, dtype=np.float32)
+        # assert a_1d_w3[t] == f(x_1d[max(0, t - 3 + 1): t + 1], period=12,
+        #                       dtype=np.float32)
+        if t >= 2:
+            assert a_1d_d2[t] == f(x_1d[: t + 1], ddof=2, period=12,
+                                   dtype=np.float32)
+
+    with pytest.raises(ValueError) as execinfo:
+        roll_f(x_1d, period=12, w=1, dtype=np.float32)
+    execinfo.match(r'1.*2')
+
+    with pytest.raises(ValueError) as execinfo:
+        roll_f(x_1d, period=12, w=2, ddof=2, dtype=np.float32)
+    execinfo.match(r'w=2.*ddof=2')
+
+    with pytest.raises(ValueError) as execinfo:
+        roll_f(x_1d, period=12, w=2, ddof=3, dtype=np.float32)
+    execinfo.match(r'w=2.*ddof=3')
+
+
+def test_roll_mad(set_variables):
+    x_1d, x_2d = set_variables
+    f = fy.mad
+    roll_f = fy.roll_mad
+    a_1d = roll_f(x_1d, dtype=np.float32)
+    a_2d = roll_f(x_2d, dtype=np.float32)
+
+    assert a_1d.dtype == np.float32
+    assert (a_1d == a_2d.flatten()).all()
+    assert a_1d.shape == (6,)
+    assert a_2d.shape == (6, 1)
+
+    a_1d_w3 = roll_f(x_1d, w=3, dtype=np.float32)
+    for t in range(1, x_1d.size):
+        assert a_1d[t] == f(x_1d[: t + 1], dtype=np.float32)
+        assert a_1d_w3[t] == f(x_1d[max(0, t - 3 + 1): t + 1], dtype=np.float32)
