fix error computation with fees and log returns

fynance/backtest/plot_tools.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+""" Functions to plot backtest. """
+
 # Built-in packages
 
 # External packages
@@ -23,12 +25,22 @@ __all__ = ['display_perf']
 # =========================================================================== #
 
 
+def compute_perf(logret, signal, fee):
+    fees = np.zeros(logret.shape)
+    fees[1:] = (signal[1:] - signal[:-1]) * fee
+    pctret = np.exp(logret) - 1 - fees
+
+    return np.cumprod(pctret * signal + 1)
+
+
 def display_perf(
     y_idx, y_est, period=252, title='', params_iv={},
     plot_drawdown=True, plot_roll_sharpe=True, x_axis=None,
     underlying='Underlying', win=252, fees=0,
 ):
-    """ Print dynamic plot of performance indicators (perf, rolling sharpe
+    """ Plot performance and print KPI of backtest.
+
+    Print dynamic plot of performance indicators (perf, rolling sharpe
     and draw down) of a strategy (raw and iso-volatility) versus its
     underlying.
 
@@ -72,16 +84,11 @@ def display_perf(
     if x_axis is None:
         x_axis = range(y_idx.size)
 
-    # Compute returns
-    vect_fee = np.zeros(y_est.shape)
-    vect_fee[1:] += np.abs(y_est[1:] - y_est[:-1]) * fees
-    vect_ret = y_idx * y_est
-
     # Compute perf.
     perf_idx = np.exp(np.cumsum(y_idx))
-    perf_est = np.exp(np.cumsum(vect_ret - vect_fee))
+    perf_est = compute_perf(y_idx, y_est, fees)
     iv = iso_vol(np.exp(np.cumsum(y_idx)), **params_iv)
-    perf_ivo = np.exp(np.cumsum((vect_ret - vect_fee) * iv))
+    perf_ivo = compute_perf(y_idx, y_est * iv, fees)
 
     # Print stats. table
     txt = set_text_stats(
@@ -136,13 +143,13 @@ def display_perf(
             'Strat Iso-Vol: {:.0f} %'.format(ax_perf.lines[1].get_ydata()[j] - 100),
             '{}: {:.0f} %'.format(underlying, ax_perf.lines[2].get_ydata()[j] - 100),
         ], loc='upper left', frameon=True, fontsize=10)
-        if plot_drawdown is not None:
+        if plot_drawdown:
             ax_dd.legend([
                 'Strategy: {:.2f} %'.format(ax_dd.lines[0].get_ydata()[j]),
                 'Strat Iso-Vol: {:.2f} %'.format(ax_dd.lines[1].get_ydata()[j]),
                 '{}: {:.2f} %'.format(underlying, ax_dd.lines[2].get_ydata()[j]),
             ], loc='upper left', frameon=True, fontsize=10)
-        if plot_roll_sharpe is not None:
+        if plot_roll_sharpe:
             ax_roll.legend([
                 'Strategy: {:.2f}'.format(ax_roll.lines[0].get_ydata()[j]),
                 'Strat Iso-Vol: {:.2f}'.format(ax_roll.lines[1].get_ydata()[j]),
@@ -158,7 +165,7 @@ def display_perf(
     ax_perf.set_title(title)
     ax_perf.tick_params(axis='x', rotation=30, labelsize=10)
     # Plot DrawDowns
-    if plot_drawdown is not None:
+    if plot_drawdown:
         ax_dd.plot(
             x_axis,
             100 * drawdown(perf_est),
@@ -181,7 +188,7 @@ def display_perf(
         ax_dd.set_title('DrawDown in percentage')
         ax_dd.tick_params(axis='x', rotation=30, labelsize=10)
     # Plot rolling Sharpe ratio
-    if plot_roll_sharpe is not None:
+    if plot_roll_sharpe:
         ax_roll.plot(
             x_axis,
             roll_sharpe(perf_est, period=period, w=win),
@@ -201,6 +208,7 @@ def display_perf(
             LineWidth=1.
         )
         ax_roll.set_ylabel('Sharpe ratio')
+        ax_roll.set_yscale('log')
         ax_roll.set_xlabel('Date')
         ax_roll.set_title('Rolling Sharpe ratio')
         ax_roll.tick_params(axis='x', rotation=30, labelsize=10)

fynance/backtest/print_stats.py
@@ -4,7 +4,7 @@
 # @Email: arthur.bernard.92@gmail.com
 # @Date: 2019-11-05 15:50:33
 # @Last modified by: ArthurBernard
-# @Last modified time: 2019-11-05 15:50:56
+# @Last modified time: 2020-05-08 09:22:17
 
 # Built-in packages
 
@@ -80,8 +80,8 @@ def set_text_stats(underly, period=252, accur=True, perf=True, vol=True,
         txt += '| {:14} | {:10.2%} |\n'.format(underlying, perf_targ)
         for key, pred in kwpred.items():
             vect_fee = np.zeros(pred.shape)
-            vect_fee[1:] += np.abs(pred[1:] - pred[:-1]) * fees
-            perf = np.exp(np.cumsum(underly * pred - vect_fee))
+            vect_fee[1:] += (pred[1:] - pred[:-1]) * fees
+            perf = np.cumprod((np.exp(underly) - 1 - vect_fee) * pred + 1)
             perf_pred = np.sign(perf[-1] / perf[0]) * np.float_power(
                 np.abs(perf[-1] / perf[0]), period / perf.size) - 1.
             txt += '| {:14} | {:10.2%} |\n'.format(key, perf_pred)
@@ -95,8 +95,8 @@ def set_text_stats(underly, period=252, accur=True, perf=True, vol=True,
         txt += '| {:14} | {:10.2%} |\n'.format(underlying, vol_targ)
         for key, pred in kwpred.items():
             vect_fee = np.zeros(pred.shape)
-            vect_fee[1:] += np.abs(pred[1:] - pred[:-1]) * fees
-            perf = np.exp(np.cumsum(underly * pred - vect_fee))
+            vect_fee[1:] += (pred[1:] - pred[:-1]) * fees
+            perf = np.cumprod((np.exp(underly) - 1 - vect_fee) * pred + 1)
             vol_pred = np.sqrt(period) * np.std(perf[1:] / perf[:-1] - 1)
             txt += '| {:14} | {:10.2%} |\n'.format(key, vol_pred)
     # Compute sharpe Ratio
@@ -108,8 +108,8 @@ def set_text_stats(underly, period=252, accur=True, perf=True, vol=True,
         txt += '| {:14} | {:10.2f} |\n'.format(underlying, sharpe_targ)
         for key, pred in kwpred.items():
             vect_fee = np.zeros(pred.shape)
-            vect_fee[1:] += np.abs(pred[1:] - pred[:-1]) * fees
-            perf = np.exp(np.cumsum(underly * pred - vect_fee))
+            vect_fee[1:] += (pred[1:] - pred[:-1]) * fees
+            perf = np.cumprod((np.exp(underly) - 1 - vect_fee) * pred + 1)
             sharpe_pred = sharpe(perf, period=period)
             txt += '| {:14} | {:10.2f} |\n'.format(key, sharpe_pred)
     # Compute calmar
@@ -121,8 +121,8 @@ def set_text_stats(underly, period=252, accur=True, perf=True, vol=True,
         txt += '| {:14} | {:10.2f} |\n'.format(underlying, calmar_targ)
         for key, pred in kwpred.items():
             vect_fee = np.zeros(pred.shape)
-            vect_fee[1:] += np.abs(pred[1:] - pred[:-1]) * fees
-            perf = np.exp(np.cumsum(underly * pred - vect_fee))
+            vect_fee[1:] += (pred[1:] - pred[:-1]) * fees
+            perf = np.cumprod((np.exp(underly) - 1 - vect_fee) * pred + 1)
             calmar_pred = calmar(perf, period=period)
             txt += '| {:14} | {:10.2f} |\n'.format(key, calmar_pred)
     txt += '+=============================+\n'