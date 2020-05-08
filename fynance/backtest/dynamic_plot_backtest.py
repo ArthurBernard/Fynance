e441f7b6d1de078ebbc415e5aeb4282736149598
ArthurBernard <arthur.bernard.92@gmail.com>
2020-05-08 20:16:17 +0200

    add object to plot backtest

--- fynance/backtest/dynamic_plot_backtest.py
+++ fynance/backtest/dynamic_plot_backtest.py
@@ -4,16 +4,14 @@
 # @Email: arthur.bernard.92@gmail.com
 # @Date: 2019-03-05 19:17:04
 # @Last modified by: ArthurBernard
-# @Last modified time: 2020-05-08 10:08:40
+# @Last modified time: 2020-05-08 20:15:27
 
 """ Module with some function plot backtest. """
 
 # Built-in packages
 
 # External packages
-import numpy as np
 from matplotlib import pyplot as plt
-import seaborn as sns
 
 # Local packages
 from fynance.backtest.plot_backtest import PlotBackTest
@@ -75,24 +73,91 @@ class DynaPlotBackTest(PlotBackTest):
         super(DynaPlotBackTest, self).__init__(fig, ax, size, True, **kwargs)
         self.ax_params = kwargs
 
-    # def _set_figure(self, fig, ax, size):
-    #    """ Set figure, axes and parameters for dynamic plot. """
-    #    PlotBackTest._set_figure(self, fig, ax, size)
-    #    plt.ion()
-    #    return self
+    def set_axes(self, **kwargs):
+        """ Set axes with initial parameters.
 
-    def set_axes(self):
-        """ Set axes with initial parameters. """
-        self._set_axes(**self.ax_params)
+        Parameters
+        ----------
+        **kwargs : keyword arguments, optioanl
+            Axes configuration, cf matplotlib documentation [1]_. By default,
+            parameters specified in __init__ method are used.
+
+        References
+        ----------
+        .. [1] https://matplotlib.org/api/axes_api.html#matplotlib.axes.Axes
+
+        """
+        ax_params = self.ax_params.copy()
+        ax_params.update(kwargs)
+        self._set_axes(**ax_params)
 
     def _set_axes(self, yscale='linear', xscale='linear', ylabel='',
                   xlabel='', title='', tick_params={}):
         """ Set axes parameters. """
         self.ax.set_yscale(yscale)
-        self.ax.set_xscale(xscale)
+        # FIXME : the below line avoid to display date on x-axis
+        # self.ax.set_xscale(xscale)
         self.ax.set_ylabel(ylabel)
         self.ax.set_xlabel(xlabel, x=0.9)
         self.ax.set_title(title)
         self.ax.tick_params(**tick_params)
 
         return self
+
+    def clear(self):
+        """ Clear axes. """
+        self.ax.clear()
+
+
+class BacktestNeuralNet:
+
+    def __init__(self, figsize=(9, 6)):
+        # Set dynamic plot object
+        self.f, (ax_1, ax_2) = plt.subplots(2, 1, figsize=figsize)
+        plt.ion()
+        self.dp_loss = DynaPlotBackTest(
+            self.f, ax_1, title='Model loss', ylabel='Loss', xlabel='Epochs',
+            yscale='log', tick_params={'axis': 'x', 'labelsize': 10}
+        )
+        self.dp_perf = DynaPlotBackTest(
+            self.f, ax_2, title='Model perf.', ylabel='Perf.',
+            xlabel='Date', yscale='log',
+            tick_params={'axis': 'x', 'rotation': 30, 'labelsize': 10}
+        )
+
+    def plot_loss(self, test, eval, train=None):
+        """ Plot loss function values for test and evaluate set. """
+        self.dp_loss.clear()
+        # Plot loss
+        self.dp_loss.plot(test, names='Test', col='BuGn', lw=2.)
+        if train is not None:
+            self.dp_loss.plot(train, names='Train', col='RdPu', lw=1.)
+
+        self.dp_loss.plot(eval, names='Eval', col='YlOrBr', loc='upper right',
+                          ncol=2, fontsize=10, handlelength=0.8,
+                          columnspacing=0.5, frameon=True, lw=1.)
+        self.dp_loss.set_axes()
+
+    def plot_perf(self, test, eval, underlying=None, index=None):
+        """ Plot performance values for test and eval set. """
+        self.dp_perf.clear()
+        if index is not None:
+            idx_test = index[-test.shape[0]:]
+            idx_eval = index[: eval.shape[0]]
+
+        else:
+            idx_test = idx_eval = None
+
+        # Plot perf of the test set
+        self.dp_perf.plot(test, x=idx_test, names='Test set', col='GnBu',
+                          lw=1.7, unit='perf')
+        # Plot perf of the eval set
+        self.dp_perf.plot(eval, x=idx_eval, names='Eval set', col='OrRd',
+                          lw=1.2, unit='perf')
+        # Plot perf of the underlying
+        if underlying is not None:
+            self.dp_perf.plot(underlying, x=idx_eval, names='Underlying',
+                              col='RdPu', lw=1.2, unit='perf')
+        self.dp_perf.set_axes()
+        self.dp_perf.ax.legend(loc='upper left', fontsize=10, frameon=True,
+                               handlelength=0.8, ncol=2, columnspacing=0.5)
