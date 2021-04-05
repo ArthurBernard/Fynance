5148c68a32052ea1a29ad5ddbe78a74bbbcfb6d1
ArthurBernard <arthur.bernard.92@gmail.com>
2021-04-05 21:08:42 +0200

    add subclass object of ndarray and small update

---
+++ fynance/core/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# @Author: ArthurBernard
+# @Email: arthur.bernard.92@gmail.com
+# @Date: 2021-04-05 20:35:18
+# @Last modified by: ArthurBernard
+# @Last modified time: 2021-04-05 20:35:21
+
+""" Description. """
+
+# Built-in packages
+
+# Third party packages
+
+# Local packages
+
+__all__ = []
+
+
+if __name__ == "__main__":
+    pass
