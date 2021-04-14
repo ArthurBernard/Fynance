d24e1ba4ccfb77e85c0b5c25df56a02c24267fdf
ArthurBernard <arthur.bernard.92@gmail.com>
2021-04-14 11:35:00 +0200

    fix docstring

--- fynance/backtest/plot.py
+++ fynance/backtest/plot.py
@@ -4,7 +4,7 @@
 # @Email: arthur.bernard.92@gmail.com
 # @Date: 2021-03-27 10:44:32
 # @Last modified by: ArthurBernard
-# @Last modified time: 2021-04-05 21:02:25
+# @Last modified time: 2021-04-14 11:21:13
 
 """ Plot objects. """
 
@@ -82,8 +82,8 @@ class PlotSeries:
             auto legends), linewidth, antialiasing, marker face color.
             Example::
 
-            >>> plot([1, 2, 3], [1, 2, 3], 'go-', label='line 1', linewidth=2)
-            >>> plot([1, 2, 3], [1, 4, 9], 'rs', label='line 2')
+            >> plot([1, 2, 3], [1, 2, 3], 'go-', label='line 1', linewidth=2)
+            >> plot([1, 2, 3], [1, 4, 9], 'rs', label='line 2')
 
             If you specify multiple lines with one plot call, the kwargs apply
             to all those lines. In case the label object is iterable, each
